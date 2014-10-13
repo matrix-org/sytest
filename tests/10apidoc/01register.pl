4dce616419e4d7246f20f7cc233a68a1586b1410
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-10-13 19:10:15 +0100

    Also test POST'ing to /register to create a new user

--- tests/10apidoc/01register.pl
+++ tests/10apidoc/01register.pl
@@ -12,6 +12,8 @@ test "GET /register yields a set of flows",
          ref $body eq "HASH" or die "Expected JSON object\n";
          ref $body->{flows} eq "ARRAY" or die "Expected 'flows' as a list\n";
 
+         my $has_register_flow;
+
          foreach my $idx ( 0 .. $#{ $body->{flows} } ) {
             my $flow = $body->{flows}[$idx];
 
@@ -20,8 +22,43 @@ test "GET /register yields a set of flows",
             #   for options that have only one stage in their flow.
             ref $flow->{stages} eq "ARRAY" or defined $flow->{type} or
                die "Expected flow[$idx] to have 'stages' as a list or a 'type'\n";
+
+            $has_register_flow++ if $flow->{type} eq "m.login.password" or
+               @{ $flow->{stages} } == 1 && $flows->{stages}[0] eq "m.login.password"
          }
 
+         $has_register_flow and
+            provide can_register_password_flow => 1;
+
          Future->done(1);
+      });
+   };
+
+test "POST /register can create a user",
+   requires => [qw( first_http_client can_register_password_flow )],
+
+   do => sub {
+      my ( $http ) = @_;
+
+      $http->do_request_json(
+         method => "POST",
+         uri    => "/register",
+
+         content => {
+            type     => "m.login.password",
+            user     => "01register-user",
+            password => "s3kr1t",
+         },
+      )->then( sub {
+         my ( $body ) = @_;
+
+         ref $body eq "HASH" or die "Expected JSON object\n";
+
+         defined $body->{$_} or die "Expected '$_'\n"
+            for qw( user_id access_token );
+
+         provide can_register => 1;
+
+         Future->done( 1 );
       });
    };
