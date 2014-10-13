d01faef01e94899dcd51599165a5f954f710d0e8
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-10-13 16:14:07 +0100

    Reduced inter-dependence of tests by moving more 'test' calls into the same files

--- tests/01register.pl
+++ tests/01register.pl
@@ -19,3 +19,21 @@ test "New users can be registered",
    },
 
    provides => [qw( users )];
+
+test "Users see their own initial presence",
+   requires => [qw( users )],
+
+   wait_time => 10,
+   check => sub {
+      my ( $USERS ) = @_;
+      foreach ( @$USERS ) {
+         my $port = $_->port;
+
+         $_->cached_presence( "\@u-$port:localhost:$port" ) eq "online" or
+            return Future->fail( "Incorrect presence for $port" );
+
+         keys %{ $_->cached_presence } > 1 and
+            return Future->fail( "Presence for $port can see too much" );
+      }
+      return Future->done(1);
+   };
