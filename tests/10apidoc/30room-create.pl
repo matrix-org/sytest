75f929f965767438325f5cf933651902706c1a12
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-10-20 16:06:10 +0100

    Test GET /publicRooms (currently failing)

--- tests/10apidoc/30room-create.pl
+++ tests/10apidoc/30room-create.pl
@@ -36,3 +36,37 @@ test "POST /createRoom makes a room",
          Future->done( scalar @{ $body->{rooms} } > 0 );
       });
    };
+
+test "GET /publicRooms lists newly-created room",
+   requires => [qw( first_http_client room_id can_create_room )],
+
+   # Currently this doesn't work - see SYN-106
+   expect_fail => 1,
+
+   check => sub {
+      my ( $http, $room_id ) = @_;
+
+      $http->do_request_json(
+         method => "GET",
+         uri    => "/publicRooms",
+      )->then( sub {
+         my ( $body ) = @_;
+
+         json_keys_ok( $body, qw( start end chunk ));
+         json_list_ok( $body->{chunk} );
+
+         my $found;
+
+         foreach my $event ( @{ $body->{chunk} } ) {
+            json_keys_ok( $event, qw( room_id ));
+            next unless $event->{room_id} eq $room_id;
+
+            $found = 1;
+         }
+
+         $found or
+            die "Failed to find our newly-created room";
+
+         Future->done(1);
+      })
+   };
