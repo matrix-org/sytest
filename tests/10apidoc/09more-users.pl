408db3be0fc2862330388c8ab5a832186c026267
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-10-24 21:07:33 +0100

    Added a variant of GET_new_events that works on other user structs

--- tests/10apidoc/09more-users.pl
+++ tests/10apidoc/09more-users.pl
@@ -2,8 +2,8 @@
 my $LOCAL_USERS = 2;
 
 # A handy little structure for other scripts to find in 'more_users'
-use Struct::Dumb 'readonly_struct';
-readonly_struct User => [qw( user_id access_token )];
+use Struct::Dumb 'struct';
+struct User => [qw( user_id access_token eventstream_token )];
 
 prepare "More local users",
    requires => [qw( first_http_client can_register )],
@@ -25,13 +25,49 @@ prepare "More local users",
             },
          )->then( sub {
             my ( $body ) = @_;
-            Future->done( User( $body->{user_id}, $body->{access_token} ) );
+            my ( $user_id, $access_token ) = @{$body}{qw( user_id access_token )};
+
+            $http->do_request_json(
+               method => "GET",
+               uri    => "/events",
+               params => { access_token => $access_token, timeout => 0 },
+            )->then( sub {
+               my ( $body ) = @_;
+
+               Future->done( User( $user_id, $access_token, $body->{end} ) );
+            });
          });
       } 1 .. $LOCAL_USERS
       )->then( sub {
          my @users = @_;
 
          provide more_users => \@users;
+
+         # A variant of GET_new_events for User() structs
+         provide GET_new_events_for_user => sub {
+            my ( $user, $filter ) = @_;
+            $filter = qr/^\Q$filter\E$/ if defined $filter and not ref $filter;
+
+            $http->do_request_json(
+               method => "GET",
+               uri    => "/events",
+               params => {
+                  access_token => $user->access_token,
+                  from         => $user->eventstream_token,
+                  timeout      => 10000,
+               }
+            )->then( sub {
+               my ( $body ) = @_;
+               $user->eventstream_token = $body->{end};
+
+               if( defined $filter ) {
+                  Future->done( grep { $_->{type} =~ $filter } @{ $body->{chunk} } );
+               }
+               else {
+                  Future->done( @{ $body->{chunk} } );
+               }
+            });
+         };
 
          Future->done();
       })
