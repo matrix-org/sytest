db94649c3e488572d75236871c79b93fdfc28211
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-10-28 16:03:23 +0000

    Check in the saved events that the initial room state arrives

--- tests/10apidoc/32room-join.pl
+++ tests/10apidoc/32room-join.pl
@@ -76,6 +76,43 @@ test "GET /events sees my join-by-ID event",
       });
    };
 
+test "Events also sees room state",
+   requires => [qw( saved_events_for_user more_users room_id
+                    can_join_room_by_id )],
+
+   check => sub {
+      my ( $saved_events_for_user, $more_users, $room_id ) = @_;
+      my $user = $more_users->[0];
+
+      $saved_events_for_user->( $user, qr/^m\.room\./ )->then( sub {
+         my @events = @_;
+
+         my %wanted = map { $_ => 0 } qw(
+            create aliases power_levels join_rules
+            add_state_level send_event_level ops_levels
+         );
+
+         foreach my $event ( @_ ) {
+            json_keys_ok( $event, qw( type ));
+            my ( $type ) = $event->{type} =~ m/^m\.room\.(.*)$/;
+
+            exists $wanted{$type} or die "Was not expecting a $event->{type} event";
+
+            # TODO: Ideally we'd only get one of each event down, but currently
+            # it seems we receive two 'm.room.aliases' events
+            $wanted{$type} and warn "Received $event->{type} multiple times";
+
+            $wanted{$type}++;
+         }
+
+         my @unreceived = grep { !$wanted{$_} } keys %wanted;
+         @unreceived and
+            die "Did not receive the expected @unreceived events";
+
+         Future->done(1);
+      });
+   };
+
 test "GET /events sees user's join-by-ID event",
    requires => [qw( GET_new_events more_users room_id
                     can_join_room_by_id )],
