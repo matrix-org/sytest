fa73d9bc17bdc557bb798c13e3ec9bca37ee1141
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-10-30 17:48:51 +0000

    Assert on presence and membership events from both sides of a room join - currently contains an expected fail due to Synapse bug

--- tests/31room-members.pl
+++ tests/31room-members.pl
@@ -51,3 +51,84 @@ test "New room members see their own join event",
          });
       } @$more_users );
    };
+
+test "New room members also see original members' presence",
+   requires => [qw( saved_events_for user more_users
+                    can_join_room_by_id )],
+
+   # Currently this test fails due to a Synapse bug. May be related to
+   #   SYN-72 or SYN-81
+   expect_fail => 1,
+   check => sub {
+      my ( $saved_events_for, $first_user, $more_users ) = @_;
+
+      Future->needs_all( map {
+         my $user = $_;
+
+         $saved_events_for->( $user, "m.presence" )->then( sub {
+            my $found;
+            foreach my $event ( @_ ) {
+               json_keys_ok( $event, qw( type content ));
+               json_keys_ok( my $content = $event->{content}, qw( user_id presence ));
+
+               next unless $content->{user_id} eq $first_user->user_id;
+
+               $found++;
+            }
+
+            $found or
+               die "Failed to find presence of existing room member";
+
+            Future->done(1);
+         });
+      } @$more_users );
+   };
+
+test "Existing members see new members' join events",
+   requires => [qw( GET_new_events_for user more_users room_id
+                    can_join_room_by_id )],
+
+   check => sub {
+      my ( $GET_new_events_for, $user, $more_users, $room_id ) = @_;
+
+      $GET_new_events_for->( $user, "m.room.member" )->then( sub {
+         my %found_user;
+         foreach my $event ( @_ ) {
+            json_keys_ok( $event, qw( type room_id user_id membership ));
+            next unless $event->{room_id} eq $room_id;
+
+            $found_user{$event->{user_id}}++;
+
+            $event->{membership} eq "join" or
+               die "Expected user membership as 'join'";
+         }
+
+         $found_user{$_->user_id} or die "Failed to find membership of ${\$_->user_id}"
+            for @$more_users;
+
+         Future->done(1);
+      });
+   };
+
+test "Existing members see new members' presence",
+   requires => [qw( saved_events_for user more_users
+                    can_join_room_by_id )],
+
+   check => sub {
+      my ( $saved_events_for, $user, $more_users ) = @_;
+
+      $saved_events_for->( $user, "m.presence" )->then( sub {
+         my %found_user;
+         foreach my $event ( @_ ) {
+            json_keys_ok( $event, qw( type content ));
+            json_keys_ok( my $content = $event->{content}, qw( user_id presence ));
+
+            $found_user{$content->{user_id}}++;
+         }
+
+         $found_user{$_->user_id} or die "Failed to find presence of ${\$_->user_id}"
+            for @$more_users;
+
+         Future->done(1);
+      });
+   };
