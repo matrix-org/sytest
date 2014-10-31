de45c174218afb662d7ebe1c7e8cb9f835353b3c
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-10-31 14:46:27 +0000

    Remember to leave the 10apidoc testing room before proceeding with the remaining tests

--- tests/10apidoc/99clean-env.pl
+++ tests/10apidoc/99clean-env.pl
@@ -1,1 +1,28 @@
+prepare "Leaving old test room",
+   requires => [qw( do_request_json_for local_users room_id
+                    can_leave_room )],
+
+   do => sub {
+      my ( $do_request_json_for, $users, $room_id ) = @_;
+
+      Future->needs_all( map {
+         my $user = $_;
+
+         $do_request_json_for->( $user,
+            method => "POST",
+            uri    => "/rooms/$room_id/leave",
+
+            content => {},
+         )->else_with_f( sub {
+            my ( $f, $failure, $name, $response ) = @_;
+
+            # Ignore 403 forbidden because of not being in the room
+            defined $name and $name eq "http" and $response->code == 403 and
+               return Future->done();
+
+            return $f;
+         });
+      } @$users );
+   };
+
 unprovide qw( room_id room_alias );
