0ac91ab21bd16f343618e580a68fc2240d7c3547
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-11-27 17:18:00 +0000

    Added initial attempt at a tiny-trivial 'cpanfile'-parsing wrapper utility to assist installing dependencies via CPAN

---
+++ install-deps.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+GetOptions(
+   'n|dryrun' => \my $DRYRUN,
+) or exit 1;
+
+sub is_installed
+{
+   my ( $mod, $want_ver ) = @_;
+
+   ( my $modfile = "$mod.pm" ) =~ s{::}{/}g;
+   return 0 unless( eval { require $modfile; 1 } );
+
+   defined $want_ver or return 1;
+   unless( $want_ver =~ s/^>=\s+// ) {
+      print STDERR "TODO: can only perform '>=' version checks\n";
+      return 1;
+   }
+
+   my $inst_ver = eval do { no strict 'refs'; ${"$mod\:\:VERSION"} };
+
+   return $inst_ver >= $want_ver;
+}
+
+sub requires
+{
+   my ( $mod, $ver ) = @_;
+
+   is_installed( $mod, $ver ) and return;
+
+   # TODO: check that some location is user-writable in @INC, and that it appears
+   # somehow in PERL_{MB,MM}_OPT
+
+   if( !$DRYRUN ) {
+      system( "cpan", $mod ) == 0 and return;
+
+      print STDERR "Failed to install $mod\n";
+      exit $? >> 8;
+   }
+   else {
+      print "cpan $mod\n";
+   }
+
+}
+
+do "cpanfile";
