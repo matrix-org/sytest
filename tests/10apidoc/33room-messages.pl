59793c56f9348333d0fc826201c1630f04a87cae
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-12-02 19:23:49 +0000

    Test /rooms/:room_id/messages

--- tests/10apidoc/33room-messages.pl
+++ tests/10apidoc/33room-messages.pl
@@ -23,3 +23,30 @@ test "POST /rooms/:room_id/send/:event_type sends a message",
          Future->done(1);
       });
    };
+
+test "GET /rooms/:room_id/messages returns a message",
+   requires => [qw( do_request_json room_id can_send_message )],
+
+   check => sub {
+      my ( $do_request_json, $room_id ) = @_;
+
+      $do_request_json->(
+         method => "GET",
+         uri    => "/rooms/$room_id/messages",
+
+         # With no params this does "forwards from END"; i.e. nothing useful
+         params => { dir => "b" },
+      )->then( sub {
+         my ( $body ) = @_;
+
+         require_json_keys( $body, qw( start end chunk ));
+         require_json_list( $body->{chunk} );
+
+         scalar @{ $body->{chunk} } > 0 or
+            die "Expected some messages but got none at all\n";
+
+         provide can_get_messages => 1;
+
+         Future->done(1);
+      });
+   };
--- tests/33room-messages.pl
+++ tests/33room-messages.pl
@@ -7,9 +7,9 @@ prepare "Flushing event streams",
    };
 
 my $msgtype = "m.message";
-my $body = "Room message for 33room-messages";
+my $msgbody = "Room message for 33room-messages";
 
-test "Local room members see posted messages",
+test "Local room members see posted message events",
    requires => [qw( do_request_json await_event_for local_users room_id
                     can_send_message )],
 
@@ -20,7 +20,7 @@ test "Local room members see posted messages",
          method => "POST",
          uri    => "/rooms/$room_id/send/m.room.message",
 
-         content => { msgtype => $msgtype, body => $body },
+         content => { msgtype => $msgtype, body => $msgbody },
       );
    },
 
@@ -42,8 +42,8 @@ test "Local room members see posted messages",
 
             $content->{msgtype} eq $msgtype or
                die "Expected msgtype as $msgtype";
-            $content->{body} eq $body or
-               die "Expected body as '$body'";
+            $content->{body} eq $msgbody or
+               die "Expected body as '$msgbody'";
             $event->{user_id} eq $senduser->user_id or
                die "Expected sender user_id as ${\$senduser->user_id}\n";
 
@@ -54,7 +54,52 @@ test "Local room members see posted messages",
       });
    };
 
-test "Remote room members also see posted messages",
+test "Local room members can get room messages",
+   requires => [qw( do_request_json_for local_users room_id
+                    can_send_message can_get_messages )],
+
+   check => sub {
+      my ( $do_request_json_for, $local_users, $room_id ) = @_;
+
+      Future->needs_all( map {
+         my $user = $_;
+
+         $do_request_json_for->( $user,
+            method => "GET",
+            uri    => "/rooms/$room_id/messages",
+
+            params => { limit => 1, dir => "b" },
+         )->then( sub {
+            my ( $body ) = @_;
+
+            require_json_keys( $body, qw( start end chunk ));
+            require_json_list( my $chunk = $body->{chunk} );
+
+            scalar @$chunk == 1 or
+               die "Expected one message";
+
+            my ( $event ) = @$chunk;
+
+            require_json_keys( $event, qw( type room_id user_id content ));
+
+            $event->{type} eq "m.room.message" or
+               die "Expected type to be m.room.message";
+            $event->{room_id} eq $room_id or
+               die "Expected room_id to be $room_id";
+
+            require_json_keys( my $content = $event->{content}, qw( msgtype body ));
+
+            $content->{msgtype} eq $msgtype or
+               die "Expected msgtype to be $msgtype";
+            $content->{body} eq $msgbody or
+               die "Expected body to be '$msgbody'";
+
+            Future->done(1);
+         });
+      } @$local_users )
+   };
+
+test "Remote room members also see posted message events",
    requires => [qw( await_event_for user remote_users room_id
                     can_receive_room_message_locally )],
 
@@ -75,12 +120,57 @@ test "Remote room members also see posted messages",
 
             $content->{msgtype} eq $msgtype or
                die "Expected msgtype as $msgtype";
-            $content->{body} eq $body or
-               die "Expected body as '$body'";
+            $content->{body} eq $msgbody or
+               die "Expected body as '$msgbody'";
             $event->{user_id} eq $senduser->user_id or
                die "Expected sender user_id as ${\$senduser->user_id}\n";
 
             return 1;
          });
       } @$remote_users );
+   };
+
+test "Remote room members can get room messages",
+   requires => [qw( do_request_json_for remote_users room_id
+                    can_send_message can_get_messages )],
+
+   check => sub {
+      my ( $do_request_json_for, $remote_users, $room_id ) = @_;
+
+      Future->needs_all( map {
+         my $user = $_;
+
+         $do_request_json_for->( $user,
+            method => "GET",
+            uri    => "/rooms/$room_id/messages",
+
+            params => { limit => 1, dir => "b" },
+         )->then( sub {
+            my ( $body ) = @_;
+
+            require_json_keys( $body, qw( start end chunk ));
+            require_json_list( my $chunk = $body->{chunk} );
+
+            scalar @$chunk == 1 or
+               die "Expected one message";
+
+            my ( $event ) = @$chunk;
+
+            require_json_keys( $event, qw( type room_id user_id content ));
+
+            $event->{type} eq "m.room.message" or
+               die "Expected type to be m.room.message";
+            $event->{room_id} eq $room_id or
+               die "Expected room_id to be $room_id";
+
+            require_json_keys( my $content = $event->{content}, qw( msgtype body ));
+
+            $content->{msgtype} eq $msgtype or
+               die "Expected msgtype to be $msgtype";
+            $content->{body} eq $msgbody or
+               die "Expected body to be '$msgbody'";
+
+            Future->done(1);
+         });
+      } @$remote_users )
    };
