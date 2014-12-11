d3d653654342be391a2d60bfc2c2e72a416ac669
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-12-11 11:05:01 +0000

    Test typing notifications

---
+++ tests/10apidoc/34room-typing.pl
@@ -0,0 +1,22 @@
+test "PUT /rooms/:room_id/typing/:user_id sets typing notification",
+   requires => [qw( do_request_json room_id
+                    can_create_room )],
+
+   do => sub {
+      my ( $do_request_json, $room_id ) = @_;
+
+      $do_request_json->(
+         method => "PUT",
+         uri    => "/rooms/$room_id/typing/:user_id",
+
+         content => { typing => 1 },
+      )->then( sub {
+         my ( $body ) = @_;
+
+         # Body is empty
+
+         provide can_set_room_typing => 1;
+
+         Future->done(1);
+      });
+   };
---
+++ tests/37room-typing.pl
@@ -0,0 +1,77 @@
+prepare "Flushing event streams",
+   requires => [qw( flush_events_for local_users remote_users )],
+   do => sub {
+      my ( $flush_events_for, $local_users, $remote_users ) = @_;
+
+      Future->needs_all( map { $flush_events_for->( $_ ) } @$local_users, @$remote_users );
+   };
+
+test "Typing notification sent to local room members",
+   requires => [qw( do_request_json await_event_for local_users room_id
+                    can_set_room_typing can_create_room can_join_room_by_id )],
+
+   do => sub {
+      my ( $do_request_json, undef, undef, $room_id ) = @_;
+
+      $do_request_json->(
+         method => "PUT",
+         uri    => "/rooms/$room_id/typing/:user_id",
+
+         content => { typing => 1, timeout => 30000 }, # msec
+      );
+   },
+
+   await => sub {
+      my ( undef, $await_event_for, $users, $room_id ) = @_;
+      my ( $typinguser ) = @$users;
+
+      Future->needs_all( map {
+         my $recvuser = $_;
+
+         $await_event_for->( $recvuser, sub {
+            my ( $event ) = @_;
+            return unless $event->{type} eq "m.typing";
+
+            require_json_keys( $event, qw( type room_id typing ));
+            require_json_list( my $typing = $event->{typing} );
+
+            return unless $event->{room_id} eq $room_id;
+
+            scalar @$typing == 1 or
+               die "Expected 1 member to be typing";
+            $typing->[0] eq $typinguser->user_id or
+               die "Expected ${\$typinguser->user_id} to be typing";
+
+            return 1;
+         })
+      } @$users );
+   };
+
+test "Typing notifications also sent to remove room members",
+   requires => [qw( await_event_for user remote_users room_id
+                    can_set_room_typing can_create_room can_join_remote_room_by_alias )],
+
+   await => sub {
+      my ( $await_event_for, $typinguser, $remote_users, $room_id ) = @_;
+
+      Future->needs_all( map {
+         my $recvuser = $_;
+
+         $await_event_for->( $recvuser, sub {
+            my ( $event ) = @_;
+            return unless $event->{type} eq "m.typing";
+
+            require_json_keys( $event, qw( type room_id typing ));
+            require_json_list( my $typing = $event->{typing} );
+
+            return unless $event->{room_id} eq $room_id;
+
+            scalar @$typing == 1 or
+               die "Expected 1 member to be typing";
+            $typing->[0] eq $typinguser->user_id or
+               die "Expected ${\$typinguser->user_id} to be typing";
+
+            return 1;
+         })
+      } @$remote_users );
+   };
