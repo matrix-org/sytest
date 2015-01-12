dfc98ae737443ffa3347ff306e4acbd58b14e160
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-01-12 15:34:13 +0000

    Test that /join/:room_id works

--- tests/10apidoc/33room-members.pl
+++ tests/10apidoc/33room-members.pl
@@ -44,7 +44,6 @@ test "POST /join/:room_alias can join a room",
       $do_request_json_for->( $user,
          method => "POST",
          uri    => "/join/$room_alias",
-         params => { access_token => $user->access_token },
 
          content => {},
       )->then( sub {
@@ -74,6 +73,92 @@ test "POST /join/:room_alias can join a room",
 
          Future->done(1);
       });
+   };
+
+test "POST /join/:room_id can join a room",
+   requires => [qw( do_request_json_for more_users room_id
+                    can_get_room_membership )],
+
+   do => sub {
+      my ( $do_request_json_for, $more_users, $room_id ) = @_;
+      my $user = $more_users->[2];
+
+      $do_request_json_for->( $user,
+         method => "POST",
+         uri    => "/join/$room_id",
+
+         content => {},
+      )->then( sub {
+         my ( $body ) = @_;
+
+         # TODO
+
+         Future->done(1);
+      });
+   },
+
+   check => sub {
+      my ( $do_request_json_for, $more_users, $room_id ) = @_;
+      my $user = $more_users->[2];
+
+      $do_request_json_for->( $user,
+         method => "GET",
+         uri    => "/rooms/$room_id/state/m.room.member/:user_id",
+      )->then( sub {
+         my ( $body ) = @_;
+
+         $body->{membership} eq "join" or
+            die "Expected membership to be 'join'";
+
+         Future->done(1);
+      });
+   };
+
+test "POST /rooms/:room_id/leave can leave a room",
+   requires => [qw( do_request_json_for more_users room_id
+                    can_join_room_by_id can_get_room_membership )],
+
+   do => sub {
+      my ( $do_request_json_for, $more_users, $room_id ) = @_;
+      my $user = $more_users->[2];
+
+      $do_request_json_for->( $user,
+         method => "POST",
+         uri    => "/rooms/$room_id/leave",
+
+         content => {},
+      );
+   },
+
+   check => sub {
+      my ( $do_request_json_for, $more_users, $room_id ) = @_;
+      my $user = $more_users->[2];
+
+      $do_request_json_for->( $user,
+         method => "GET",
+         uri    => "/rooms/$room_id/state/m.room.member/:user_id",
+      )->then(
+         sub { # then
+            my ( $body ) = @_;
+
+            $body->{membership} eq "join" and
+               die "Expected membership not to be 'join'";
+
+            provide can_leave_room => 1;
+
+            Future->done(1);
+         },
+         sub { # else
+            my ( $failure, $name, $response ) = @_;
+            Future->fail( @_ ) unless defined $name and $name eq "http";
+            Future->fail( @_ ) unless $response->code == 403;
+
+            # We're expecting a 403 so that's fine
+            provide can_leave_room => 1;
+
+            Future->done(1);
+         },
+      );
    };
 
 test "POST /rooms/:room_id/invite can send an invite",
@@ -109,51 +194,4 @@ test "POST /rooms/:room_id/invite can send an invite",
 
          Future->done(1);
       });
-   };
-
-test "POST /rooms/:room_id/leave can leave a room",
-   requires => [qw( do_request_json_for more_users room_id
-                    can_join_room_by_id can_get_room_membership )],
-
-   do => sub {
-      my ( $do_request_json_for, $more_users, $room_id ) = @_;
-      my $user = $more_users->[0];
-
-      $do_request_json_for->( $user,
-         method => "POST",
-         uri    => "/rooms/$room_id/leave",
-
-         content => {},
-      );
-   },
-
-   check => sub {
-      my ( $do_request_json_for, $more_users, $room_id ) = @_;
-      my $user = $more_users->[0];
-
-      $do_request_json_for->( $user,
-         method => "GET",
-         uri    => "/rooms/$room_id/state/m.room.member/:user_id",
-      )->then(
-         sub { # then
-            my ( $body ) = @_;
-
-            $body->{membership} eq "join" and
-               die "Expected membership not to be 'join'";
-
-            provide can_leave_room => 1;
-
-            Future->done(1);
-         },
-         sub { # else
-            my ( $failure, $name, $response ) = @_;
-            Future->fail( @_ ) unless defined $name and $name eq "http";
-            Future->fail( @_ ) unless $response->code == 403;
-
-            # We're expecting a 403 so that's fine
-            provide can_leave_room => 1;
-
-            Future->done(1);
-         },
-      );
    };
