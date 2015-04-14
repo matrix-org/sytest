698d39a23af89afd76c0ccfdecaf932731dd4494
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-04-14 17:04:43 +0100

    Also test at the 10apidoc stage that m.room.power_levels can be set

--- tests/10apidoc/36room-levels.pl
+++ tests/10apidoc/36room-levels.pl
@@ -1,10 +1,10 @@
 test "GET /rooms/:room_id/state/m.room.power_levels can fetch levels",
-   requires => [qw( do_request_json user more_users room_id )],
+   requires => [qw( do_request_json user room_id )],
 
    provides => [qw( can_get_power_levels )],
 
    check => sub {
-      my ( $do_request_json, $user, $more_users, $room_id ) = @_;
+      my ( $do_request_json, $user, $room_id ) = @_;
 
       $do_request_json->(
          method => "GET",
@@ -30,6 +30,44 @@ test "GET /rooms/:room_id/state/m.room.power_levels can fetch levels",
             die "Expected room creator to have a higher-than-default powerlevel";
 
          provide can_get_power_levels => 1;
+         Future->done(1);
+      });
+   };
+
+test "PUT /rooms/:room_id/state/m.room.power_levels can set levels",
+   requires => [qw( do_request_json user more_users room_id
+                    can_get_power_levels )],
+
+   provides => [qw( can_set_power_levels )],
+
+   do => sub {
+      my ( $do_request_json, $user, $more_users, $room_id ) = @_;
+
+      $do_request_json->(
+         method => "GET",
+         uri    => "/rooms/$room_id/state/m.room.power_levels",
+      )->then( sub {
+         my ( $levels ) = @_;
+
+         $levels->{users}{'@random-other-user:their.home'} = 20;
+
+         $do_request_json->(
+            method => "PUT",
+            uri    => "/rooms/$room_id/state/m.room.power_levels",
+            content => $levels,
+         )
+      })->then( sub {
+         $do_request_json->(
+            method => "GET",
+            uri    => "/rooms/$room_id/state/m.room.power_levels",
+         )
+      })->then( sub {
+         my ( $levels ) = @_;
+
+         $levels->{users}{'@random-other-user:their.home'} == 20 or
+            die "Expected to have set other user's level to 20";
+
+         provide can_set_power_levels => 1;
          Future->done(1);
       });
    };
