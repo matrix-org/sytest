1dbc7fddb649cc3fdbede3eaf9de533651464006
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-04-14 14:42:47 +0100

    Factor out the ban-based powerlevel test into a reüseable test function so we can write other tests

--- tests/38room-levels.pl
+++ tests/38room-levels.pl
@@ -20,24 +20,25 @@ my $set_user_powerlevel = sub {
    })
 };
 
-multi_test "'ban' event respects room powerlevel",
-   requires => [qw( do_request_json_for user local_users room_id
-                    can_ban_room )],
+sub test_powerlevel
+{
+   my ( $name, %args ) = @_;
 
-   do => sub {
-      my ( $do_request_json_for, $user, $local_users, $room_id ) = @_;
-      my $test_user = $local_users->[1];
+   my $do = $args{do};
 
-      # Fails at powerlevel 0
-      $set_user_powerlevel->( $do_request_json_for, $user, $room_id,
-         $test_user->user_id, 0
-      )->then( sub {
-         $do_request_json_for->( $test_user,
-            method => "POST",
-            uri    => "/rooms/$room_id/ban",
+   multi_test $name,,
+      requires => [qw( do_request_json_for user local_users room_id ), @{ $args{requires} } ],
 
-            content => { user_id => '@random_dude:test', reason => "testing" },
-         )->then(
+      do => sub {
+         my ( $do_request_json_for, $user, $local_users, $room_id ) = @_;
+         my $test_user = $local_users->[1];
+
+         # Fails at powerlevel 0
+         $set_user_powerlevel->( $do_request_json_for, $user, $room_id,
+            $test_user->user_id, 0
+         )->then( sub {
+            $do->( $do_request_json_for, $test_user, $room_id );
+         })->then(
             sub { # done
                Future->fail( "Expected to fail at powerlevel=0 but it didn't" );
             },
@@ -48,22 +49,31 @@ multi_test "'ban' event respects room powerlevel",
 
                return Future->fail( @_ );
             },
-         )
-      })->then( sub {
-         pass( "Fails at powerlevel 0" );
+         )->then( sub {
+            pass( "Fails at powerlevel 0" );
 
-         # Succeeds at powerlevel 100
-         $set_user_powerlevel->( $do_request_json_for, $user, $room_id,
-            $test_user->user_id, 100
-         )
-      })->then( sub {
-         $do_request_json_for->( $test_user,
-            method => "POST",
-            uri    => "/rooms/$room_id/ban",
+            # Succeeds at powerlevel 100
+            $set_user_powerlevel->( $do_request_json_for, $user, $room_id,
+               $test_user->user_id, 100
+            )
+         })->then( sub {
+            $do->( $do_request_json_for, $test_user, $room_id );
+         })->on_done( sub {
+            pass( "Succeeds at powerlevel 100" );
+         })
+      };
+}
 
-            content => { user_id => '@random_dude:test', reason => "testing" },
-         );
-      })->on_done( sub {
-         pass( "Succeeds at powerlevel 100" );
-      })
+test_powerlevel "'ban' event respects room powerlevel",
+   requires => [qw( can_ban_room )],
+
+   do => sub {
+      my ( $do_request_json_for, $test_user, $room_id ) = @_;
+
+      $do_request_json_for->( $test_user,
+         method => "POST",
+         uri    => "/rooms/$room_id/ban",
+
+         content => { user_id => '@random_dude:test', reason => "testing" },
+      );
    };
