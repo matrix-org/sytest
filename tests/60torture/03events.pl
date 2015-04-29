6dc8959493a5646e11ffa85fd67659d1f1464e96
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-04-29 18:21:37 +0100

    Perform a torture test on a numeric parameter using a negative value

--- tests/60torture/03events.pl
+++ tests/60torture/03events.pl
@@ -28,6 +28,21 @@ test "GET /events with non-numeric 'limit'",
       )->$expect_http_4xx;
    };
 
+test "GET /events with negative 'limit'",
+   requires => [qw( do_request_json_for user expect_http_4xx
+                    can_get_events )],
+
+   check => sub {
+      my ( $do_request_json_for, $user, $expect_http_4xx ) = @_;
+
+      $do_request_json_for->( $user,
+         method => "GET",
+         uri    => "/events",
+
+         params => { from => $user->eventstream_token, limit => -2 },
+      )->$expect_http_4xx;
+   };
+
 test "GET /events with non-numeric 'timeout'",
    requires => [qw( do_request_json_for user expect_http_4xx
                     can_get_events )],
