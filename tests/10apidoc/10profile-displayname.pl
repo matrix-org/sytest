63084c97164b055a425cb55e19ec0643e7df9d31
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-06-08 19:54:51 +0100

    Provide 'can_get_displayname' from GET displayname test

--- tests/10apidoc/10profile-displayname.pl
+++ tests/10apidoc/10profile-displayname.pl
@@ -41,6 +41,8 @@ test "PUT /profile/:user_id/displayname sets my name",
 test "GET /profile/:user_id/displayname publicly accessible",
    requires => [qw( first_http_client user can_set_displayname )],
 
+   provides => [qw( can_get_displayname )],
+
    check => sub {
       my ( $http, $user ) = @_;
       my $user_id = $user->user_id;
@@ -56,6 +58,8 @@ test "GET /profile/:user_id/displayname publicly accessible",
 
          $body->{displayname} eq $displayname or
             die "Expected displayname to be '$displayname'";
+
+         provide can_get_displayname => 1;
 
          Future->done(1);
       });
