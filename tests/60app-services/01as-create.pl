21d5c36279f66816b029f2dc7d6c07e133d4fc55
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-06-09 17:09:24 +0100

    Also test that the AS token cannot register names outside its space, and that the namespace is exclusive

--- tests/60app-services/01as-create.pl
+++ tests/60app-services/01as-create.pl
@@ -22,3 +22,30 @@ test "AS can create a user",
          Future->done(1);
       });
    };
+
+test "AS cannot create users outside its own namespace",
+   requires => [qw( do_request_json_for as_user expect_http_4xx )],
+
+   do => sub {
+      my ( $do_request_json_for, $as_user, $expect_http_4xx ) = @_;
+
+      $do_request_json_for->( $as_user,
+         method => "POST",
+         uri    => "/register",
+
+         content => {
+            type => "m.login.application_service",
+            user => "a-different-user",
+         }
+      )->$expect_http_4xx;
+   };
+
+test "Regular users cannot register within the AS namespace",
+   requires => [qw( register_new_user first_http_client expect_http_4xx )],
+
+   do => sub {
+      my ( $register_new_user, $http, $expect_http_4xx ) = @_;
+
+      $register_new_user->( $http, "astest-01create-2" )
+         ->$expect_http_4xx;
+   };
