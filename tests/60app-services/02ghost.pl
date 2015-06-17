d6f4e67ac78cf85e594dee28ba74bdb9ebca3040
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-06-17 16:58:15 +0100

    Test AS-ghosting both as the ghost itself and via the AS

--- tests/60app-services/02ghost.pl
+++ tests/60app-services/02ghost.pl
@@ -1,9 +1,9 @@
-multi_test "AS-ghosted users can use rooms",
-   requires => [qw( make_test_room make_as_user do_request_json_for await_event_for user
+multi_test "AS-ghosted users can use rooms via AS",
+   requires => [qw( make_test_room make_as_user do_request_json_for await_event_for user as_user
                     can_join_room_by_id can_receive_room_message_locally )],
 
    do => sub {
-      my ( $make_test_room, $make_as_user, $do_request_json_for, $await_event_for, $user ) = @_;
+      my ( $make_test_room, $make_as_user, $do_request_json_for, $await_event_for, $user, $as_user ) = @_;
 
       my $room_id;
       my $ghost;
@@ -19,6 +19,74 @@ multi_test "AS-ghosted users can use rooms",
 
          pass "Created AS ghost";
 
+         $do_request_json_for->( $as_user,
+            method => "POST",
+            uri    => "/rooms/$room_id/join",
+            params => {
+               user_id => $ghost->user_id,
+            },
+
+            content => {},
+         )
+      })->then( sub {
+         pass "User joined room via AS";
+
+         $do_request_json_for->( $as_user,
+            method => "POST",
+            uri    => "/rooms/$room_id/send/m.room.message",
+            params => {
+               user_id => $ghost->user_id,
+            },
+
+            content => { msgtype => "m.text", body => "Message from AS directly" },
+         )
+      })->then( sub {
+         pass "User posted message via AS";
+
+         $await_event_for->( $user, sub {
+            my ( $event ) = @_;
+            return unless $event->{type} eq "m.room.message";
+            return unless $event->{room_id} eq $room_id;
+
+            log_if_fail "Event", $event;
+
+            my $content = $event->{content};
+
+            $content->{body} eq "Message from AS directly" or
+               die "Expected 'body' as 'Message from AS directly'";
+            $event->{user_id} eq $ghost->user_id or
+               die "Expected sender user_id as ${\$ghost->user_id}";
+
+            return 1;
+         })
+      })->then( sub {
+         pass "Creator received user's message";
+
+         Future->done(1);
+      });
+   };
+
+multi_test "AS-ghosted users can use rooms themselves",
+   requires => [qw( make_test_room make_as_user do_request_json_for await_event_for user
+                    can_join_room_by_id can_receive_room_message_locally )],
+
+   do => sub {
+      my ( $make_test_room, $make_as_user, $do_request_json_for, $await_event_for, $user ) = @_;
+
+      my $room_id;
+      my $ghost;
+
+      $make_test_room->( $user )->then( sub {
+         ( $room_id ) = @_;
+
+         pass "Created test room";
+
+         $make_as_user->( "02ghost-2" )
+      })->then( sub {
+         ( $ghost ) = @_;
+
+         pass "Created AS ghost";
+
          $do_request_json_for->( $ghost,
             method => "POST",
             uri    => "/rooms/$room_id/join",
@@ -26,7 +94,7 @@ multi_test "AS-ghosted users can use rooms",
             content => {},
          )
       })->then( sub {
-         pass "Ghost joined room";
+         pass "Ghost joined room themselves";
 
          $do_request_json_for->( $ghost,
             method => "POST",
@@ -35,7 +103,7 @@ multi_test "AS-ghosted users can use rooms",
             content => { msgtype => "m.text", body => "Message from AS Ghost" },
          )
       })->then( sub {
-         pass "Ghost posted message";
+         pass "Ghost posted message themselves";
 
          $await_event_for->( $user, sub {
             my ( $event ) = @_;
