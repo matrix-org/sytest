83454b0ccf9ab9fd635d9855283fdf8cbaa88160
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-06-26 19:13:31 +0100

    Assert that the received HS->AS token is correct

--- tests/60app-services/00prepare.pl
+++ tests/60app-services/00prepare.pl
@@ -13,12 +13,12 @@ prepare "Creating special AS user",
    };
 
 prepare "Creating test helper functions",
-   requires => [qw( await_http_request )],
+   requires => [qw( await_http_request hs2as_token )],
 
    provides => [qw( await_as_event )],
 
    do => sub {
-      my ( $await_http_request ) = @_;
+      my ( $await_http_request, $hs2as_token ) = @_;
 
       provide await_as_event => sub {
          my ( $type ) = @_;
@@ -33,6 +33,14 @@ prepare "Creating test helper functions",
 
             # Respond immediately to AS
             $request->respond_json( {} );
+
+            my $uri = $request->as_http_request->uri;
+            my %query_params = $uri->query_form;
+
+            defined $query_params{access_token} or
+               die "Expected HS to provide an access_token";
+            $query_params{access_token} eq $hs2as_token or
+               die "HS did not provide the correct token";
 
             my ( $event ) = grep { $_->{type} eq $type } @{ $body->{events} };
             Future->done( $event );
