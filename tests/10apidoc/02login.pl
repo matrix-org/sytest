594acfa9df80000aff18e6795a48688377b1058b
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-09-01 14:49:31 +0100

    'can_register' should just be a boolean; provide the login details in a different key called 'login_details'

--- tests/10apidoc/02login.pl
+++ tests/10apidoc/02login.pl
@@ -40,7 +40,8 @@ test "GET /login yields a set of flows",
    };
 
 test "POST /login can log in as a user",
-   requires => [qw( first_v1_client can_register can_login_password_flow )],
+   requires => [qw( first_v1_client login_details
+                    can_login_password_flow )],
 
    provides => [qw( can_login user first_home_server do_request_json_for do_request_json )],
 
@@ -97,7 +98,8 @@ test "POST /login can log in as a user",
    };
 
 test "POST /login wrong password is rejected",
-   requires => [qw( first_v1_client can_register can_login_password_flow )],
+   requires => [qw( first_v1_client login_details
+                    can_login_password_flow )],
 
    do => sub {
       my ( $http, $login_details ) = @_;
