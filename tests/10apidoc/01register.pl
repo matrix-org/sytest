b95094167f2ca1a0254cd133568a3460fbd69a6e
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-09-07 16:48:29 +0100

    Declare the 'User' struct in 01register.pl where the helper function that first makes use of it is

--- tests/10apidoc/01register.pl
+++ tests/10apidoc/01register.pl
@@ -1,3 +1,6 @@
+# A handy little structure for other scripts to find in 'user' and 'more_users'
+struct User => [qw( http user_id access_token refresh_token eventstream_token saved_events pending_get_events )];
+
 test "GET /register yields a set of flows",
    requires => [qw( first_v1_client )],
 
