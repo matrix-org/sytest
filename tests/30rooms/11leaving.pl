a30933108c88ce7a4aab24e5556753e40b5f7819
Mark Haines <mark.haines@matrix.org>
2015-09-08 18:19:30 +0100

    Test that rooms that we have left are included in initialSync

--- tests/30rooms/11leaving.pl
+++ tests/30rooms/11leaving.pl
@@ -11,16 +11,16 @@ multi_test "Setup a room, and havve the first user leave (SPEC-216)",
     # User A creates a room.
     # User A invites User B to the room.
     # User B joins the room.
-    # User B will send a message with body "M1. B's first message"
-    # User B will send a message with body "M2. B's second message"
     # User B will set the ("madeup.test.state", "") state of the room to {
-    #   "body": "S1. B's first state"
+    #   "body": "S1. B's state before A left"
     # }
+    # User B will send a message with body "M1. B's message before A left"
+    # User B will send a message with body "M2. B's message before A left"
     # User A will leave the room.
     # User B will set the ("madeup.test.state", "") state of the room to {
-    #   "body": "S2. B's second state"
+    #   "body": "S2. B's state after A left"
     # }
-    # User B will send a message with text "M3. B's third message"
+    # User B will send a message with text "M3. B's message after A left"
     #
     do => sub {
         my (
@@ -42,10 +42,16 @@ multi_test "Setup a room, and havve the first user leave (SPEC-216)",
             })
         })->then( sub {
             $do_request_json_for->( $user_b,
+                method => "PUT",
+                uri => "/rooms/$room_id/state/madeup.test.state",
+                content => { "body" => "S1. B's state before A left", },
+            )->on_done(sub { pass "User B set the state the first time" })
+        })->then( sub {
+            $do_request_json_for->( $user_b,
                 method => "POST",
                 uri => "/rooms/$room_id/send/m.room.message",
                 content => {
-                    "body" => "M1. B's first message",
+                    "body" => "M1. B's message before A left",
                     "msgtype" => "m.room.text",
                 },
             )->on_done(sub { pass "User B sent their first message" })
@@ -54,16 +60,10 @@ multi_test "Setup a room, and havve the first user leave (SPEC-216)",
                 method => "POST",
                 uri => "/rooms/$room_id/send/m.room.message",
                 content => {
-                    "body" => "M2. B's second message",
+                    "body" => "M2. B's message before A left",
                     "msgtype" => "m.room.text",
                 },
             )->on_done(sub { pass "User B sent their second message" })
-        })->then( sub {
-            $do_request_json_for->( $user_b,
-                method => "PUT",
-                uri => "/rooms/$room_id/state/madeup.test.state",
-                content => { "body" => "S1. B's first state", },
-            )->on_done(sub { pass "User B set the state the first time" })
         })->then( sub {
             $do_request_json_for->( $user_a,
                 method => "POST",
@@ -75,7 +75,7 @@ multi_test "Setup a room, and havve the first user leave (SPEC-216)",
                 method => "POST",
                 uri => "/rooms/$room_id/send/m.room.message",
                 content => {
-                    "body" => "M3. B's third message",
+                    "body" => "M3. B's message after A left",
                     "msgtype" => "m.room.text",
                 },
             )->on_done(sub { pass "User B sent their third message" })
@@ -83,7 +83,7 @@ multi_test "Setup a room, and havve the first user leave (SPEC-216)",
             $do_request_json_for->( $user_b,
                 method => "PUT",
                 uri => "/rooms/$room_id/state/madeup.test.state",
-                content => { "body" => "S1. B's second state", },
+                content => { "body" => "S2. B's state after A left", },
             )->on_done(sub { pass "User B set the state the second time" })
         })
     };
@@ -97,17 +97,33 @@ test "A departed room is still included in /initialSync (SPEC-216)",
         $do_request_json->(
             method => "GET",
             uri => "/initialSync",
-            params => { limit => 1 },
+            params => { limit => 2 },
         )->then( sub {
             my ( $body ) = @_;
 
             require_json_keys( $body, qw( rooms ) );
 
-            my $room = grep { $_->{room_id} eq $departed_room_id }
+            my ( $room ) = grep { $_->{room_id} eq $departed_room_id }
                 @{$body->{rooms}};
 
             die "Departed room not in /initialSync"
                 unless $room;
+
+            require_json_keys( $room, qw( state messages membership ) );
+
+            die "Membership is not leave"
+                unless $room->{membership} eq "leave";
+
+            my ( $madeup_test_state ) = grep { $_->{type} eq "madeup.test.state" }
+                @{$room->{state}};
+
+            die "Received state that happened after leaving the room"
+                unless $madeup_test_state->{content}{body}
+                    eq "S1. B's state before A left";
+
+            die "Received message that happened after leaving the room"
+                unless $room->{messages}{chunk}[0]{content}{body}
+                    eq "M2. B's message before A left";
 
             Future->done(1);
         })
