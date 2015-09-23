cb670e55edbaa0935eb8759008e52bb68057a61d
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-09-23 18:01:23 +0100

    Neater argument unpacking of member list to make_test_room

--- tests/10apidoc/33room-members.pl
+++ tests/10apidoc/33room-members.pl
@@ -261,15 +261,13 @@ prepare "Creating test-room-creation helper function",
       my ( $do_request_json_for, $await_event_for ) = @_;
 
       provide make_test_room => sub {
-         # First member is the room creator
          my ( $members, %options ) = @_;
+         my ( $creator, @other_members ) = @$members;
 
          $options{visibility} = "public" unless exists $options{visibility};
 
          my $room_id;
          my $room_alias_shortname = "test-$next_alias"; $next_alias++;
-         my $creator = @$members[0];
-         my @other_members = @$members[1 .. $#$members];
 
          my ( $domain ) = $creator->user_id =~ m/:(.*)$/;
          my $room_alias_fullname = "#${room_alias_shortname}:$domain";
