2905f77afc36747a4a1470166fab955af313e4a0
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-09-30 16:53:57 +0100

    Have do_request_json_for() print a slightly more useful error message on bad usage

--- tests/10apidoc/00prepare.pl
+++ tests/10apidoc/00prepare.pl
@@ -3,9 +3,18 @@ our @EXPORT = qw( User do_request_json_for );
 # A handy little structure for other scripts to find in 'user' and 'more_users'
 struct User => [qw( http user_id access_token refresh_token eventstream_token saved_events pending_get_events )];
 
+*do_request_json_for = \&MatrixAPIHelpers::do_request_json_for;
+
+# Put these API helpers in their own package so that croak works nicely
+package MatrixAPIHelpers;
+
+use Carp;
+
 sub do_request_json_for
 {
    my ( $user, %args ) = @_;
+
+   $user or croak 'Expected a "$user" to do_request_json_for';
 
    my $user_id = $user->user_id;
    ( my $uri = delete $args{uri} ) =~ s/:user_id/$user_id/g;
