8f9625565f03592842c94aa7bbe465a8edd1bc4d
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-10-02 16:38:31 +0100

    do_request_json_for() is now a function

--- tests/90jira/SYN-482.pl
+++ tests/90jira/SYN-482.pl
@@ -1,30 +1,30 @@
 multi_test "Limit on room/initialSync is reached over federation (SYN-482)",
-    requires => [qw( do_request_json_for make_test_room local_users remote_users)],
+    requires => [qw( make_test_room local_users remote_users)],
 
     check => sub {
-        my ( $do_request_json_for, $make_test_room, $local_users, $remote_users ) = @_;
+        my ( $make_test_room, $local_users, $remote_users ) = @_;
         my $user_a = $local_users->[0];
         my $user_b = $remote_users->[0];
         my $room_id;
         my $messages_events;
         my $sync_events;
 
-        $make_test_room->( $user_a )->then( sub {
+        $make_test_room->( [ $user_a ] )->then( sub {
             ( $room_id ) = @_;
-            $do_request_json_for->( $user_a,
+            do_request_json_for( $user_a,
                 method  => "PUT",
                 uri     => "/api/v1/rooms/$room_id/state/m.room.history_visibility",
                 content => { history_visibility => "invited" },
             )
         })->then( sub {
-            $do_request_json_for->( $user_a,
+            do_request_json_for( $user_a,
                 method  => "POST",
                 uri     => "/api/v1/rooms/$room_id/invite",
                 content => { user_id => $user_b->user_id },
             )
         })->then( sub {
             Future->needs_all(map {
-                $do_request_json_for->( $user_a,
+                do_request_json_for( $user_a,
                     method  => "POST",
                     uri     => "/api/v1/rooms/$room_id/send/m.room.message",
                     content => {
@@ -34,13 +34,13 @@ multi_test "Limit on room/initialSync is reached over federation (SYN-482)",
                 )
             } 1..3)
         })->then( sub {
-            $do_request_json_for->( $user_b,
+            do_request_json_for( $user_b,
                 method  => "POST",
                 uri     => "/api/v1/rooms/$room_id/join",
                 content => {},
             )
         })->then( sub {
-            $do_request_json_for->( $user_b,
+            do_request_json_for( $user_b,
                 method => "GET",
                 uri    => "/api/v1/rooms/$room_id/initialSync",
                 params => {limit => 10},
@@ -48,7 +48,7 @@ multi_test "Limit on room/initialSync is reached over federation (SYN-482)",
         })->then( sub {
             my ($body) = @_;
             $sync_events = $body->{messages}->{chunk};
-            $do_request_json_for->( $user_b,
+            do_request_json_for( $user_b,
                 method => "GET",
                 uri    => "/api/v1/rooms/$room_id/messages",
                 params => {limit => 10, dir => "b"},
