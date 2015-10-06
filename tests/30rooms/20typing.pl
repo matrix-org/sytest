8c87ccefb9a0cd34de97ef7be74bc7337ce8bf40
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-10-06 20:42:53 +0100

    Run 30rooms/20typing.pl on freshly prepared remote user and room

--- tests/30rooms/20typing.pl
+++ tests/30rooms/20typing.pl
@@ -1,38 +1,27 @@
 use Time::HiRes qw( time );
 
-prepare "Flushing event streams",
-   requires => [qw( local_users remote_users )],
-   do => sub {
-      my ( $local_users, $remote_users ) = @_;
-
-      Future->needs_all(
-         map { flush_events_for( $_ ) } @$local_users, @$remote_users
-      );
-   };
-
-my $room_id;
 my @local_members;
 
-prepare "Creating test room",
-   requires => [qw( local_users remote_users )],
+my $remote_user_preparer = remote_user_preparer();
+
+my $room_preparer = preparer(
+   requires => [qw( local_users ), $remote_user_preparer ],
 
    do => sub {
-      my ( $local_users, $remote_users ) = @_;
+      my ( $local_users, $remote_user ) = @_;
 
       @local_members = @$local_users;
 
-      matrix_create_and_join_room( [ @$local_users, @$remote_users ] )
-      ->on_done( sub {
-         ( $room_id ) = @_;
-      });
-   };
+      matrix_create_and_join_room( [ @$local_users, $remote_user ] )
+   },
+);
 
 test "Typing notification sent to local room members",
-   requires => [qw( user
-                    can_set_room_typing )],
+   requires => [qw( user ), $room_preparer,
+                qw( can_set_room_typing )],
 
    do => sub {
-      my ( $typinguser ) = @_;
+      my ( $typinguser, $room_id ) = @_;
 
       do_request_json_for( $typinguser,
          method => "PUT",
@@ -66,42 +55,38 @@ test "Typing notification sent to local room members",
    };
 
 test "Typing notifications also sent to remove room members",
-   requires => [qw( user remote_users
-                    can_set_room_typing can_join_remote_room_by_alias )],
+   requires => [qw( user ), $remote_user_preparer, $room_preparer,
+                qw( can_set_room_typing can_join_remote_room_by_alias )],
 
    do => sub {
-      my ( $typinguser, $remote_users ) = @_;
+      my ( $typinguser, $remote_user, $room_id ) = @_;
 
-      Future->needs_all( map {
-         my $recvuser = $_;
+      await_event_for( $remote_user, sub {
+         my ( $event ) = @_;
+         return unless $event->{type} eq "m.typing";
 
-         await_event_for( $recvuser, sub {
-            my ( $event ) = @_;
-            return unless $event->{type} eq "m.typing";
+         require_json_keys( $event, qw( type room_id content ));
+         require_json_keys( my $content = $event->{content}, qw( user_ids ));
 
-            require_json_keys( $event, qw( type room_id content ));
-            require_json_keys( my $content = $event->{content}, qw( user_ids ));
+         return unless $event->{room_id} eq $room_id;
 
-            return unless $event->{room_id} eq $room_id;
+         require_json_list( my $users = $content->{user_ids} );
 
-            require_json_list( my $users = $content->{user_ids} );
+         scalar @$users == 1 or
+            die "Expected 1 member to be typing";
+         $users->[0] eq $typinguser->user_id or
+            die "Expected ${\$typinguser->user_id} to be typing";
 
-            scalar @$users == 1 or
-               die "Expected 1 member to be typing";
-            $users->[0] eq $typinguser->user_id or
-               die "Expected ${\$typinguser->user_id} to be typing";
-
-            return 1;
-         })
-      } @$remote_users );
+         return 1;
+      })
    };
 
 test "Typing can be explicitly stopped",
-   requires => [qw( user
-                    can_set_room_typing )],
+   requires => [qw( user ), $room_preparer,
+                qw( can_set_room_typing )],
 
    do => sub {
-      my ( $typinguser ) = @_;
+      my ( $typinguser, $room_id ) = @_;
 
       do_request_json_for( $typinguser,
          method => "PUT",
@@ -133,11 +118,11 @@ test "Typing can be explicitly stopped",
    };
 
 multi_test "Typing notifications timeout and can be resent",
-   requires => [qw( user
-                    can_set_room_typing )],
+   requires => [qw( user ), $room_preparer,
+                qw( can_set_room_typing )],
 
    do => sub {
-      my ( $user ) = @_;
+      my ( $user, $room_id ) = @_;
 
       my $start_time = time();
 
