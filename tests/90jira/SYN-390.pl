da461ffbf1b16c9dc2799d5b1b3f2215d5af6099
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-10-06 15:38:53 +0100

    Call prepare_local_user/users in 90jira tests where appropriate

--- tests/90jira/SYN-390.pl
+++ tests/90jira/SYN-390.pl
@@ -1,29 +1,21 @@
+my $user = prepare_local_user;
+
 multi_test "Getting push rules doesn't corrupt the cache SYN-390",
-   requires => [qw( api_clients )],
-
    do => sub {
-      my ( $clients ) = @_;
-      my $http = $clients->[0];
+      do_request_json_for( $user,
+         method  => "PUT",
+         uri     => "/api/v1/pushrules/global/sender/%40a_user%3Amatrix.org",
+         content => { "actions" => ["dont_notify"] }
+      )->SyTest::pass_on_done("Set push rules for user" )
+      ->then( sub {
 
-      my $alice;
-
-      matrix_register_user( $http )->then( sub {
-         ( $alice ) = @_;
-
-         do_request_json_for( $alice,
-            method  => "PUT",
-            uri     => "/api/v1/pushrules/global/sender/%40a_user%3Amatrix.org",
-            content => { "actions" => ["dont_notify"] }
-         )->SyTest::pass_on_done("Set push rules for alice" )
-      })->then( sub {
-
-         do_request_json_for( $alice,
+         do_request_json_for( $user,
             method => "GET",
             uri    => "/api/v1/pushrules/",
          )->SyTest::pass_on_done("Got push rules the first time" )
       })->then( sub {
 
-         do_request_json_for( $alice,
+         do_request_json_for( $user,
             method => "GET",
             uri    => "/api/v1/pushrules/",
          )->SyTest::pass_on_done("Got push rules the second time" )
