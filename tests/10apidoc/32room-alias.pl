6536e27ee3f6df034b55bdb02e3dddd20e5409c1
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-10-09 19:07:11 +0100

    Use an inline room preparer instead of 'prepare' block in 10apidoc/32room-alias

--- tests/10apidoc/32room-alias.pl
+++ tests/10apidoc/32room-alias.pl
@@ -1,26 +1,22 @@
 my $alias_localpart = "#another-alias";
 
-my $room_id;
-
-prepare "Creating test room",
+my $room_preparer = preparer(
    requires => [qw( user )],
 
    do => sub {
       my ( $user ) = @_;
 
-      matrix_create_room( $user )
-      ->on_done( sub {
-         ( $room_id ) = @_;
-      });
-   };
+      matrix_create_room( $user );
+   },
+);
 
 test "PUT /directory/room/:room_alias creates alias",
-   requires => [qw( user first_home_server )],
+   requires => [qw( user first_home_server ), $room_preparer ],
 
    provides => [qw( can_create_room_alias can_lookup_room_alias )],
 
    do => sub {
-      my ( $user, $first_home_server ) = @_;
+      my ( $user, $first_home_server, $room_id ) = @_;
       my $room_alias = "${alias_localpart}:$first_home_server";
 
       do_request_json_for( $user,
@@ -36,7 +32,7 @@ test "PUT /directory/room/:room_alias creates alias",
    },
 
    check => sub {
-      my ( $user, $first_home_server ) = @_;
+      my ( $user, $first_home_server, $room_id ) = @_;
       my $room_alias = "${alias_localpart}:$first_home_server";
 
       do_request_json_for( $user,
