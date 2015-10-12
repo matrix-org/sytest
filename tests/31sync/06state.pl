bdece04c05e1f74973bbcb58fd6e1b1eea2e38dc
Mark Haines <mark.haines@matrix.org>
2015-10-12 18:16:30 +0100

    Check that the delta to the state is included in an incremental sync.

--- tests/31sync/06state.pl
+++ tests/31sync/06state.pl
@@ -3,7 +3,7 @@ test "That state is included in the initial sync",
 
     check => sub {
         my ( $http ) = @_;
-        my ( $user, $filter_id, $room_id, $event_id_1 );
+        my ( $user, $filter_id, $room_id );
 
         my $filter = {
             room => {
@@ -35,6 +35,66 @@ test "That state is included in the initial sync",
             $room->{event_map}{$event_id}{type} eq "a.madeup.test.state"
                 or die "Unexpected state event type";
             $room->{event_map}{$event_id}{content}{my_key} == 1
+                or die "Unexpected event content";
+            Future->done(1)
+        })
+    };
+
+test "That changes to state are included in an incremental sync",
+    requires => [qw( first_api_client can_sync )],
+
+    check => sub {
+        my ( $http ) = @_;
+        my ( $user, $filter_id, $room_id, $next_batch );
+
+        my $filter = {
+            room => {
+                timeline => { types => [] },
+                state => { types => ["a.madeup.test.state"] },
+                ephemeral => { types => [] },
+            },
+            presence => {types => [] },
+        };
+
+        matrix_register_user_with_filter( $http, $filter )->then( sub {
+            ( $user,  $filter_id ) = @_;
+            matrix_create_room( $user )
+        })->then( sub {
+            ( $room_id ) = @_;
+            Future->needs_all(
+                matrix_put_room_state( $user, $room_id,
+                    type => "a.madeup.test.state",
+                    content => { "my_key" => 1 },
+                    state_key => "this_state_changes"
+                ),
+                matrix_put_room_state( $user, $room_id,
+                    type => "a.madeup.test.state",
+                    content => { "my_key" => 1 },
+                    state_key => "this_state_does_not_change"
+                ),
+            )
+        })->then( sub {
+            matrix_sync( $user, filter => $filter_id );
+        })->then( sub {
+            my ( $body ) = @_;
+            $next_batch = $body->{next_batch};
+            matrix_put_room_state( $user, $room_id,
+                type => "a.madeup.test.state",
+                content => { "my_key" => 2 },
+                state_key => "this_state_changes",
+            )
+        })->then( sub {
+            matrix_sync( $user, filter => $filter_id, since => $next_batch)
+        })->then( sub {
+            my ( $body ) = @_;
+            my $room = $body->{rooms}{joined}{$room_id};
+            require_json_keys( $room, qw( event_map timeline state ephemeral ));
+            @{$room->{state}{events}} == 1
+                or die "Expected only one state event";
+            my $event_id = $room->{state}{events}[0];
+            $room->{event_map}{$event_id}{type} eq "a.madeup.test.state"
+                or die "Unexpected state event type";
+            $room->{event_map}{$event_id}{content}{my_key} == 2
                 or die "Unexpected event content";
             Future->done(1)
         })
