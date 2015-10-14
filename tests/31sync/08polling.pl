27b7f9c3b0c0fa0f88ad119454342710815ab18d
Mark Haines <mark.haines@matrix.org>
2015-10-14 11:51:10 +0100

    That sync can be polled for updates and is woken up by new messages

---
+++ tests/31sync/08polling.pl
@@ -0,0 +1,36 @@
+test "That sync can be polled for updates",
+    requires => [qw( first_api_client can_sync )],
+
+    check => sub {
+        my ( $http ) = @_;
+        my ( $user, $filter_id, $room_id, $next );
+
+        matrix_register_user_with_filter( $http, {} )->then( sub {
+            ( $user, $filter_id ) = @_;
+            matrix_create_room( $user )
+        })->then( sub {
+            ( $room_id ) = @_;
+            matrix_sync( $user, filter => $filter_id )
+        })->then( sub {
+            my ( $body ) = @_;
+            $next = $body->{next_batch};
+            Future->wait_any(
+                Future->needs_all(
+                    matrix_sync( $user,
+                        filter => $filter_id, since => $next, timeout => 10000
+                    ),
+                    delay( 0.1 )->then( sub {
+                        matrix_send_room_text_message(
+                            $user, $room_id, body => "1"
+                        )
+                    })
+                ),
+                delay( 10 )->then_fail("Timeout waiting for sync")
+            )
+        })->then( sub {
+            my ( $body, $response, $event_id ) = @_;
+            $body->{rooms}{joined}{$room_id}{timeline}{events}[0] eq $event_id
+                or die "Unexpected timeline event";
+            Future->done(1)
+        })
+    };
