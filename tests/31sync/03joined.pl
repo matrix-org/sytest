1a92758f52c0e8c632363e17f059fc1ffa45fbb3
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-10-15 14:58:39 +0100

    STYLE-GUIDE: Reindent to sw=3, add blank lines after @_ and splitting major blocks; terminating semicolon on final statements; misc. whitespace

--- tests/31sync/03joined.pl
+++ tests/31sync/03joined.pl
@@ -1,66 +1,86 @@
 test "Can sync a joined room",
-    requires => [qw( first_api_client can_sync )],
+   requires => [qw( first_api_client can_sync )],
 
-    check => sub {
-        my ( $http ) = @_;
-        my ( $user, $filter_id, $room_id );
-        my $filter = { room => { timeline => { limit => 10 } } };
-        matrix_register_user_with_filter( $http, $filter )->then( sub {
-            ( $user, $filter ) = @_;
-            matrix_create_room( $user )
-        })->then( sub {
-            ( $room_id ) = @_;
-            matrix_sync( $user, filter => $filter_id )
-        })->then( sub {
-            my ( $body ) = @_;
-            my $room = $body->{rooms}{joined}{$room_id};
-            require_json_keys( $room, qw( event_map timeline state ephemeral ));
-            require_json_keys( $room->{timeline}, qw( events limited prev_batch ));
-            require_json_keys( $room->{state}, qw( events ));
-            require_json_keys( $room->{ephemeral}, qw( events ));
-            require_json_keys( $room->{event_map}, @{ $room->{timeline}{events} } );
-            require_json_keys( $room->{event_map}, @{ $room->{state}{events} } );
-            matrix_sync( $user, filter => $filter_id, since => $body->{next_batch} );
-        })->then( sub {
-            my ( $body ) = @_;
-            my $room = $body->{rooms}{joined}{$room_id};
-            (!defined $room) or die "Unchanged rooms shouldn't be in the sync response";
-            Future->done(1)
-        })
-    };
+   check => sub {
+      my ( $http ) = @_;
+
+      my ( $user, $filter_id, $room_id );
+
+      my $filter = { room => { timeline => { limit => 10 } } };
+
+      matrix_register_user_with_filter( $http, $filter )->then( sub {
+         ( $user, $filter ) = @_;
+
+         matrix_create_room( $user )
+      })->then( sub {
+         ( $room_id ) = @_;
+
+         matrix_sync( $user, filter => $filter_id )
+      })->then( sub {
+         my ( $body ) = @_;
+
+         my $room = $body->{rooms}{joined}{$room_id};
+         require_json_keys( $room, qw( event_map timeline state ephemeral ));
+         require_json_keys( $room->{timeline}, qw( events limited prev_batch ));
+         require_json_keys( $room->{state}, qw( events ));
+         require_json_keys( $room->{ephemeral}, qw( events ));
+         require_json_keys( $room->{event_map}, @{ $room->{timeline}{events} } );
+         require_json_keys( $room->{event_map}, @{ $room->{state}{events} } );
+
+         matrix_sync( $user, filter => $filter_id, since => $body->{next_batch} );
+      })->then( sub {
+         my ( $body ) = @_;
+
+         my $room = $body->{rooms}{joined}{$room_id};
+         (!defined $room) or die "Unchanged rooms shouldn't be in the sync response";
+
+         Future->done(1)
+      })
+   };
 
 
 test "Newly joined room is included in an incremental sync",
-    requires => [qw( first_api_client can_sync )],
+   requires => [qw( first_api_client can_sync )],
 
-    check => sub {
-        my ( $http ) = @_;
-        my ( $user, $filter_id, $room_id, $next_batch);
-        my $filter = { room => { timeline => { limit => 10 } } };
-        matrix_register_user_with_filter( $http, $filter )->then( sub {
-            ( $user, $filter_id ) = @_;
-            matrix_sync( $user, filter => $filter_id )
-        })->then( sub {
-            my ( $body ) = @_;
-            $next_batch = $body->{next_batch};
-            matrix_create_room( $user )
-        })->then( sub {
-            ( $room_id ) = @_;
-            matrix_sync( $user, filter => $filter_id, since => $next_batch )
-        })->then( sub {
-            my ( $body ) = @_;
-            my $room = $body->{rooms}{joined}{$room_id};
-            require_json_keys( $room, qw( event_map timeline state ephemeral ));
-            require_json_keys( $room->{timeline}, qw( events limited prev_batch ));
-            require_json_keys( $room->{state}, qw( events ));
-            require_json_keys( $room->{ephemeral}, qw( events ));
-            require_json_keys( $room->{event_map}, @{ $room->{timeline}{events} } );
-            require_json_keys( $room->{event_map}, @{ $room->{state}{events} } );
-            matrix_sync( $user, filter => $filter_id, since => $body->{next_batch} );
-        })->then( sub {
-            my ( $body ) = @_;
-            my $room = $body->{rooms}{joined}{$room_id};
-            (!defined $room) or die "Unchanged rooms shouldn't be in the sync response";
-            Future->done(1)
-        })
-    };
+   check => sub {
+      my ( $http ) = @_;
+
+      my ( $user, $filter_id, $room_id, $next_batch );
+
+      my $filter = { room => { timeline => { limit => 10 } } };
+
+      matrix_register_user_with_filter( $http, $filter )->then( sub {
+         ( $user, $filter_id ) = @_;
+
+         matrix_sync( $user, filter => $filter_id );
+      })->then( sub {
+         my ( $body ) = @_;
+
+         $next_batch = $body->{next_batch};
+
+         matrix_create_room( $user );
+      })->then( sub {
+         ( $room_id ) = @_;
+
+         matrix_sync( $user, filter => $filter_id, since => $next_batch );
+      })->then( sub {
+         my ( $body ) = @_;
+
+         my $room = $body->{rooms}{joined}{$room_id};
+         require_json_keys( $room, qw( event_map timeline state ephemeral ));
+         require_json_keys( $room->{timeline}, qw( events limited prev_batch ));
+         require_json_keys( $room->{state}, qw( events ));
+         require_json_keys( $room->{ephemeral}, qw( events ));
+         require_json_keys( $room->{event_map}, @{ $room->{timeline}{events} } );
+         require_json_keys( $room->{event_map}, @{ $room->{state}{events} } );
+
+         matrix_sync( $user, filter => $filter_id, since => $body->{next_batch} );
+      })->then( sub {
+         my ( $body ) = @_;
+
+         my $room = $body->{rooms}{joined}{$room_id};
+         (!defined $room) or die "Unchanged rooms shouldn't be in the sync response";
+
+         Future->done(1)
+      })
+   };
