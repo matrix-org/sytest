29e2123b0b40f1dc20e4b32cca9e5b0467df52d2
Daniel Wagner-Hall <daniel@matrix.org>
2015-10-26 13:16:18 +0000

    Add some tests for event size limits

--- tests/80torture/03events.pl
+++ tests/80torture/03events.pl
@@ -54,3 +54,32 @@ test "GET /events with non-numeric 'timeout'",
          params => { from => $user->eventstream_token, timeout => "hello" },
       )->main::expect_http_4xx;
    };
+
+test "Event size limits",
+   requires => do {
+      my $user = local_user_preparer();
+      [ $user, room_preparer( requires_users => [ $user ] ) ];
+   },
+
+   do => sub {
+      my ( $user, $room_id ) = @_;
+
+      Future->needs_all(
+         do_request_json_for( $user,
+            method  => "POST",
+            uri     => "/api/v1/rooms/$room_id/send/m.room.message",
+            content => {
+               msgtype => "m.text",
+               body    => "A" x 70000,
+            },
+         )->followed_by( \&main::expect_http_413 ),
+
+         do_request_json_for( $user,
+            method  => "PUT",
+            uri     => "/api/v1/rooms/$room_id/state/oooooooh/",
+            content => {
+               key => "O" x 70000,
+            }
+         )->followed_by( \&main::expect_http_413 ),
+      );
+   };
