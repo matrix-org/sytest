f18c3d0ddce2f9119557ae363fb9e81fbfa5e730
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-11-09 18:47:07 +0000

    Move the JSON:: hackery into its own file

---
+++ lib/SyTest/JSONSensible.pm
@@ -0,0 +1,26 @@
+package SyTest::JSONSensible;
+
+use Data::Dump::Filtered;
+
+## TERRIBLY RUDE but it seems to work
+package JSON::number {
+   use overload '0+' => sub { ${ $_[0] } },
+                fallback => 1;
+   sub new {
+      my ( $class, $value ) = @_;
+      return bless \$value, $class;
+   }
+
+   # By this even more terrible hack we can be both a function name and a package
+   sub JSON::number { JSON::number::->new( $_[0] ) }
+
+   sub TO_JSON { 0 + ${ $_[0] } }
+
+   Data::Dump::Filtered::add_dump_filter( sub {
+      ( ref($_[1]) // '' ) eq __PACKAGE__
+         ? { dump => "JSON::number(${ $_[1] })" }
+         : undef;
+   });
+}
+
+1;
