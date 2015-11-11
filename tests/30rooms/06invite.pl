697f79c65c335c9dfbf01a6fb63c0cf6ca4ffce7
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-11-11 17:53:53 +0000

    Assert that a received room invite message contains the room state list (SYT-20)

--- tests/30rooms/06invite.pl
+++ tests/30rooms/06invite.pl
@@ -1,4 +1,4 @@
-use List::Util qw( first );
+use List::Util qw( all first );
 
 my $creator_fixture = local_user_fixture();
 
@@ -87,6 +87,9 @@ test "Invited user receives invite",
          $content->{membership} eq "invite" or
             die "Expected membership to be 'invite'";
 
+         require_json_keys( $event, qw( invite_room_state ));
+         require_json_list( $event->{invite_room_state} );
+
          return 1;
       });
    };
@@ -154,3 +157,76 @@ sub invited_user_can_reject_invite
       Future->done(1);
    });
 }
+
+test "Invited user can see room metadata",
+   requires => [ local_user_and_room_fixtures(), local_user_fixture() ],
+
+   do => sub {
+      my ( $creator, $room_id, $invitee ) = @_;
+
+      my $state_in_invite;
+
+      Future->needs_all(
+         matrix_put_room_state( $creator, $room_id,
+            type => "m.room.name",
+            content => { name => "The room name" },
+         ),
+         matrix_put_room_state( $creator, $room_id,
+            type => "m.room.avatar",
+            content => { url => "http://something" },
+         ),
+      )->then( sub {
+         matrix_invite_user_to_room( $creator, $invitee, $room_id );
+      })->then( sub {
+         await_event_for( $invitee, sub {
+            my ( $event ) = @_;
+            return $event->{type} eq "m.room.member" &&
+                   $event->{room_id} eq $room_id;
+         });
+      })->then( sub {
+         my ( $event ) = @_;
+
+         require_json_keys( $event, qw( invite_room_state ));
+         my %state_by_type = map {
+            $_->{type} => $_
+         } @{ $event->{invite_room_state} };
+
+         $state_by_type{$_} or die "Did not receive $_ state"
+            for qw( m.room.join_rules m.room.name m.room.canonical_alias m.room.avatar );
+
+         # Save just the 'content' keys
+         $state_in_invite = { map {
+            $_ => $state_by_type{$_}{content}
+         } keys %state_by_type };
+
+         log_if_fail "State content in invite", $state_in_invite;
+
+         # Now compare it to the actual room state
+
+         Future->needs_all(
+            map {
+               my $type = $_;
+               matrix_get_room_state( $creator, $room_id, type => $type )
+                  ->then( sub {
+                     my ( $content ) = @_;
+                     Future->done( $type => $content );
+                  });
+            } keys %state_by_type
+         )
+      })->then( sub {
+         my %got_state = @_;
+
+         log_if_fail "State by direct room query", \%got_state;
+
+         # TODO: This would be a lot neater with is_deeply()
+         foreach my $type ( keys %got_state ) {
+            my $got       = $got_state{$type};
+            my $in_invite = $state_in_invite->{$type};
+
+            all { $got->{$_} eq $in_invite->{$_} } keys %$got, keys %$in_invite or
+               die "Content does not match for type $type";
+         }
+
+         Future->done(1);
+      });
+   };
