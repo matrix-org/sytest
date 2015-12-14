60b40d4ec98517eb8571e8c21cd2a63924d48589
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-12-14 16:26:19 +0000

    Add some POD docs to SyTest::Federation::Room

--- lib/SyTest/Federation/Room.pm
+++ lib/SyTest/Federation/Room.pm
@@ -3,10 +3,28 @@ package SyTest::Federation::Room;
 use strict;
 use warnings;
 
+=head1 NAME
+
+C<SyTest::Federation::Room> - represent a single Room instance
+
+=cut
+
 sub make_event_refs
 {
    [ map { [ $_->{event_id}, $_->{hashes} ] } @_ ];
 }
+
+=head1 CONSTRUCTOR
+
+=cut
+
+=head2 new
+
+   $room = SyTest::Federation::Room->new( room_id => $room_id )
+
+Constructs a new Room instance, initially blank containing no state or events.
+
+=cut
 
 sub new
 {
@@ -23,10 +41,36 @@ sub new
    }, $class;
 }
 
+=head1 METHODS
+
+=cut
+
+=head2 room_id
+
+   $room_id = $room->room_id
+
+Accessor to return the room ID.
+
+=cut
+
 sub room_id
 {
    return $_[0]->{room_id};
 }
+
+=head2 create_initial_events
+
+   $room->create_initial_events( server => $server, creator => $creator )
+
+Convenience helper method to create all the initial state events required at
+room creation time. It supplies the C<m.room.create> event with the given
+C<$creator> user ID as the room's creator, the C<m.room.member> event for this
+user, and a C<m.room.join_rules> to set the room join permission as C<public>.
+
+C<$server> is an instance of L<SyTest::Federation::Server> required to
+actually create and persist the individual events.
+
+=cut
 
 sub create_initial_events
 {
@@ -66,9 +110,20 @@ sub create_initial_events
       sender      => $creator,
       state_key   => "",
    );
-
-   return $self;
 }
+
+=head2 create_event
+
+   $event = $room->create_event( server => $server, %fields )
+
+Constructs a new event in the room and updates the current state, if it is a
+state event. This helper also fills in the C<prev_events> and C<auth_events>
+lists, meaning the caller does not have to.
+
+C<$server> is an instance of L<SyTest::Federation::Server> required to
+actually create and persist the event.
+
+=cut
 
 sub create_event
 {
@@ -101,11 +156,30 @@ sub create_event
    return $event;
 }
 
+=head2 current_state_events
+
+   @events = $room->current_state_events
+
+Returns a list of events, in no particular order, that comprises the complete
+current state; that is, the latest value of any event with a C<state_key>
+field.
+
+=cut
+
 sub current_state_events
 {
    my $self = shift;
    return values %{ $self->{current_state} };
 }
+
+=head2 get_current_state_event
+
+   $event = $room->get_current_state_event( $type, $state_key )
+
+Returns the latest state event for the given C<$type> and optional
+C<$state_key>, or C<undef> if there is none.
+
+=cut
 
 sub get_current_state_event
 {
@@ -115,6 +189,19 @@ sub get_current_state_event
 
    return $self->{current_state}{ join "\0", $type, $state_key };
 }
+
+=head2 make_join_protoevent
+
+   $protoevent = $room->make_join_protoevent( user_id => $user_id )
+
+Returns a HASH reference containing most of the fields required to form the
+protoevent response to a C</make_join> federation request to this room, if the
+given C<$user_id> wishes to join it. The caller will have to supply the
+C<origin> and C<origin_server_ts> fields before sending it back to the
+requesting client. As a new HASH reference is returned by each call, the
+caller is free to modify it inplace as required.
+
+=cut
 
 sub make_join_protoevent
 {
