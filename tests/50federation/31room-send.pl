533fff84ef31c0580eeb48e5ff672255dbf870d7
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-12-16 21:53:32 +0000

    Added an inbound federation test to check that SUT can receive transactions with room events in them

--- tests/50federation/31room-send.pl
+++ tests/50federation/31room-send.pl
@@ -43,3 +43,51 @@ test "Outbound federation can send events",
          );
       });
    };
+
+test "Inbound federation can receive events",
+   requires => [ $main::OUTBOUND_CLIENT, $main::HOMESERVER_INFO[0],
+                 local_user_and_room_fixtures() ],
+
+   do => sub {
+      my ( $outbound_client, $info, $user, $room_id ) = @_;
+      my $first_home_server = $info->server_name;
+
+      my $local_server_name = $outbound_client->server_name;
+
+      my $user_id = "\@50fed-user:$local_server_name";
+
+      $outbound_client->join_room(
+         server_name => $first_home_server,
+         room_id     => $room_id,
+         user_id     => $user_id,
+      )->then( sub {
+         my ( $room ) = @_;
+
+         my $event = $room->create_event(
+            type => "m.room.message",
+
+            sender  => $user_id,
+            content => {
+               body => "Hello",
+            },
+         );
+
+         $outbound_client->send_event(
+            event => $event,
+            destination => $first_home_server,
+         );
+      })->then( sub {
+         await_event_for( $user, filter => sub {
+            my ( $event ) = @_;
+            return unless $event->{type} eq "m.room.message";
+            return unless $event->{room_id} eq $room_id;
+
+            assert_eq( $event->{sender}, $user_id,
+               'event sender' );
+            assert_eq( $event->{content}{body}, "Hello",
+               'event content body' );
+
+            Future->done(1);
+         });
+      });
+   };
