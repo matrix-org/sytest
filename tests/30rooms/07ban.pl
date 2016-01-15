30f57dc96b28cb3065697e777215c8e43c9d491e
Daniel Wagner-Hall <dawagner@gmail.com>
2016-01-15 15:42:17 +0000

    Test that unban is required after a ban

--- tests/30rooms/07ban.pl
+++ tests/30rooms/07ban.pl
@@ -2,7 +2,7 @@ my $creator_fixture = local_user_fixture();
 
 my $banned_user_fixture = local_user_fixture();
 
-test "Banned user is kicked and may not rejoin",
+test "Banned user is kicked and may not rejoin until unbanned",
    requires => [ $creator_fixture, $banned_user_fixture,
                      room_fixture( requires_users => [ $creator_fixture, $banned_user_fixture ] ),
                 qw( can_ban_room )],
@@ -26,6 +26,34 @@ test "Banned user is kicked and may not rejoin",
             die "Expected banned user membership to be 'ban'";
 
          matrix_join_room( $banned_user, $room_id )
-            ->main::expect_http_403;
-      });
+            ->main::expect_http_403;  # Must be unbanned first
+      })->then( sub {
+         do_request_json_for( $creator,
+            method => "POST",
+            uri    => "/api/v1/rooms/$room_id/invite",
+
+            content => { user_id => $banned_user->user_id },
+         )->main::expect_http_403;  # Must be unbanned first
+      })->then( sub {
+         do_request_json_for( $creator,
+            method => "POST",
+            uri    => "/api/v1/rooms/$room_id/kick",
+
+            content => { user_id => $banned_user->user_id },
+         )->main::expect_http_403;  # Must be unbanned first
+      })->then( sub {
+         do_request_json_for( $creator,
+            method => "POST",
+            uri    => "/api/v1/rooms/$room_id/unban",
+
+            content => { user_id => $banned_user->user_id },
+         );
+      })->then( sub {
+         do_request_json_for( $banned_user,
+            method => "POST",
+            uri    => "/api/v1/rooms/$room_id/join",
+
+            content => {},
+         );
+      })
    };
