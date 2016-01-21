5b97a273524b1f67c83cb308e5662f28b8b51380
Erik Johnston <erikj@matrix.org>
2016-01-21 16:35:15 +0000

    Add test for non-joined users peeking into multiple rooms

--- tests/30rooms/60anonymousaccess.pl
+++ tests/30rooms/60anonymousaccess.pl
@@ -100,6 +100,8 @@ foreach my $i (
 
             matrix_set_room_history_visibility( $user, $room_id, "world_readable" );
          })->then( sub {
+            matrix_initialsync_room( $nonjoined_user, $room_id )
+         })->then( sub {
             Future->needs_all(
                matrix_send_room_text_message( $user, $room_id, body => "mice" )
                ->on_done( sub {
@@ -190,6 +192,58 @@ foreach my $i (
          });
       },
    );
+
+   test(
+      "Real user can call /events on another world_readable room",
+      requires => [ $fixture->( with_events => 0 ),
+                    local_user_fixture( with_events => 0 ) ],
+
+      do => sub {
+         my ( $nonjoined_user, $user ) = @_;
+
+         my ( $room_id1, $room_id2 );
+
+         Future->needs_all(
+            matrix_create_and_join_room( [ $user ] ),
+            matrix_create_and_join_room( [ $user ] ),
+         )->then( sub {
+            ( $room_id1, $room_id2 ) = @_;
+
+            Future->needs_all(
+               matrix_set_room_history_visibility( $user, $room_id1, "world_readable" ),
+               matrix_set_room_history_visibility( $user, $room_id2, "world_readable" ),
+            )
+         })->then( sub {
+            matrix_initialsync_room( $nonjoined_user, $room_id1 )
+         })->then( sub {
+            Future->needs_all(
+               matrix_send_room_text_message( $user, $room_id1, body => "moose" ),
+               await_event_not_history_visibility_or_presence_for( $nonjoined_user, $room_id1, [] ),
+            );
+         })->then( sub {
+            matrix_initialsync_room( $nonjoined_user, $room_id2 )
+         })->then( sub {
+            Future->needs_all(
+               delay( 0.1 )->then( sub {
+                  matrix_send_room_text_message( $user, $room_id2, body => "mice" );
+               }),
+
+               await_event_not_history_visibility_or_presence_for( $nonjoined_user, $room_id2, [] )
+               ->then( sub {
+                  my ( $event ) = @_;
+
+                  assert_json_keys( $event, qw( content ) );
+                  my $content = $event->{content};
+                  assert_json_keys( $content, qw( body ) );
+                  $content->{body} eq "mice" or die "Want content body to be mice";
+
+                  Future->done( 1 );
+               }),
+            );
+         });
+      },
+   );
+
 
    test(
       "$name non-joined user doesn't get events before room made world_readable",
