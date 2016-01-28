2172bc3f2a65b4110f66fe476fc49f47885fb735
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-01-28 18:02:35 +0000

    Move the implementation of APPSERV await_event into ApplicationService.pm

--- lib/SyTest/ApplicationService.pm
+++ lib/SyTest/ApplicationService.pm
@@ -3,15 +3,55 @@ package SyTest::ApplicationService;
 use strict;
 use warnings;
 
+use Future::Utils qw( repeat );
+
 sub new
 {
    my $class = shift;
-   my ( $info, $await_http, $await_event ) = @_;
+   my ( $info, $await_http ) = @_;
+
+   my $path = $info->path;
+
+   # Map event types to ARRAYs of Futures
+   my %futures_by_type;
+
+   my $f = repeat {
+      $await_http->( qr{^\Q$path\E/transactions/\d+$}, sub { 1 },
+         timeout => 0,
+      )->then( sub {
+         my ( $request ) = @_;
+
+         # Respond immediately to AS
+         $request->respond_json( {} );
+
+         foreach my $event ( @{ $request->body_from_json->{events} } ) {
+            my $type = $event->{type};
+
+            my $queue = $futures_by_type{$type};
+
+            # Ignore any cancelled ones
+            shift @$queue while $queue and @$queue and $queue->[0]->is_cancelled;
+
+            if( $queue and my $f = shift @$queue ) {
+               $f->done( $event, $request );
+            }
+            else {
+               warn "Ignoring incoming AS event of type $type\n";
+            }
+         }
+
+         Future->done;
+      })
+   } while => sub { not $_[0]->failure };
+
+   $f->on_fail( sub { die $_[0] } );
 
    return bless {
       info       => $info,
       await_http => $await_http,
-      await_event => $await_event,
+
+      futures_by_type => \%futures_by_type,
+      await_loop_f    => $f,
    }, $class;
 }
 
@@ -26,8 +66,20 @@ sub await_http_request
 sub await_event
 {
    my $self = shift;
+   my ( $type ) = @_;
 
-   $self->{await_event}->( @_ );
+   my $failmsg = SyTest::CarpByFile::shortmess(
+      "Timed out waiting for an AS event of type $type"
+   );
+
+   push @{ $self->{futures_by_type}{$type} }, my $f = Future->new;
+
+   return Future->wait_any(
+      $f,
+
+      main::delay( 10 )
+         ->then_fail( $failmsg ),
+   );
 }
 
 1;
--- tests/60app-services/00prepare.pl
+++ tests/60app-services/00prepare.pl
@@ -1,5 +1,3 @@
-use Future::Utils qw( repeat );
-
 use SyTest::ApplicationService;
 
 push our @EXPORT, qw( AS_USER APPSERV );
@@ -15,60 +13,6 @@ our $AS_USER = fixture(
    },
 );
 
-# Map event types to ARRAYs of Futures
-my %futures_by_type;
-
-sub await_as_event
-{
-   my ( $type ) = @_;
-   my $failmsg = SyTest::CarpByFile::shortmess(
-      "Timed out waiting for an AS event of type $type"
-   );
-
-   push @{ $futures_by_type{$type} }, my $f = $loop->new_future;
-
-   return Future->wait_any(
-      $f,
-
-      delay( 10 )
-         ->then_fail( $failmsg ),
-   );
-}
-
-my $f = repeat {
-   await_http_request( qr{^/appserv/transactions/\d+$}, sub { 1 },
-      timeout => 0,
-   )->then( sub {
-      my ( $request ) = @_;
-
-      # Respond immediately to AS
-      $request->respond_json( {} );
-
-      foreach my $event ( @{ $request->body_from_json->{events} } ) {
-         my $type = $event->{type};
-
-         my $queue = $futures_by_type{$type};
-
-         # Ignore any cancelled ones
-         shift @$queue while $queue and @$queue and $queue->[0]->is_cancelled;
-
-         if( $queue and my $f = shift @$queue ) {
-            $f->done( $event, $request );
-         }
-         else {
-            warn "Ignoring incoming AS event of type $type\n";
-         }
-      }
-
-      Future->done;
-   })
-} while => sub { not $_[0]->failure };
-
-$f->on_fail( sub { die $_[0] } );
-
-# lifecycle it
-$f->on_cancel( sub { undef $f } );
-
 our $APPSERV = fixture(
    requires => [ $main::AS_INFO ],
 
@@ -76,7 +20,7 @@ our $APPSERV = fixture(
       my ( $info ) = @_;
 
       Future->done( SyTest::ApplicationService->new(
-         $info, \&main::await_http_request, \&await_as_event,
+         $info, \&main::await_http_request
       ) );
    }
 );
