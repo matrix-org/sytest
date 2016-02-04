ae456e47b7e8a6380da9c99b5b18a1cd22019b00
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-02-04 18:32:25 +0000

    Various minor whitespace/style fixes

--- tests/90jira/SYN-482.pl
+++ tests/90jira/SYN-482.pl
@@ -12,24 +12,24 @@ multi_test "Limit on room/initialSync is reached over federation (SYN-482)",
 
       matrix_set_room_history_visibility( $user_a, $room_id, "invited" )
       ->then( sub {
-         matrix_invite_user_to_room( $user_a, $user_b, $room_id )
+         matrix_invite_user_to_room( $user_a, $user_b, $room_id );
       })->then( sub {
-         Future->needs_all(map {
+         Future->needs_all( map {
             matrix_send_room_text_message( $user_a, $room_id,
                body => "Message #$_",
             )
-         } 1..3)
+         } 1..3 );
       })->then( sub {
-         matrix_join_room( $user_b, $room_id )
+         matrix_join_room( $user_b, $room_id );
       })->then( sub {
          matrix_initialsync_room( $user_b, $room_id, limit => 10 );
       })->then( sub {
-         my ($body) = @_;
+         my ( $body ) = @_;
          $sync_events = $body->{messages}->{chunk};
 
          matrix_get_room_messages( $user_b, $room_id, limit => 10 );
       })->then( sub {
-         my ($body) = @_;
+         my ( $body ) = @_;
 
          $messages_events = ${body}->{chunk};
 
