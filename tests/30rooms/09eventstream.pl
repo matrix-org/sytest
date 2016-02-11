44e07539a07152bddd4852bd5c67019ca355f479
Erik Johnston <erikj@matrix.org>
2016-02-11 10:09:18 +0000

    Test the order of /events stream

--- tests/30rooms/09eventstream.pl
+++ tests/30rooms/09eventstream.pl
@@ -1,3 +1,6 @@
+use Future::Utils qw( repeat try_repeat );
+
+
 multi_test "Check that event streams started after a client joined a room work (SYT-1)",
    requires => [ local_user_fixture( with_events => 0 ),
       qw( can_create_private_room can_send_message )
@@ -36,4 +39,68 @@ multi_test "Check that event streams started after a client joined a room work (
             return 1;
          })->SyTest::pass_on_done( "Alice saw her message" )
       })->then_done(1);
+   };
+
+
+test "Event stream catches up fully after many messages",
+   requires => [ local_user_fixture( with_events => 0 ),
+                 qw( can_send_message )],
+
+   do => sub {
+      my ( $user ) = @_;
+      my ( $room_id, @expected_event_ids );
+
+      matrix_create_room( $user,
+        visibility => "private",
+      )
+      ->then( sub {
+         ( $room_id ) = @_;
+
+         flush_events_for( $user )
+      })
+      ->then( sub {
+         repeat( sub {
+            my ( $msgnum ) = @_;
+
+            matrix_send_room_text_message( $user, $room_id,
+               body => "Message number $msgnum"
+            )
+            ->on_done( sub {
+               push @expected_event_ids, @_;
+            })
+         }, foreach => [ 0 .. 19 ] )
+      })
+      ->then( sub {
+         try_repeat( sub {
+            matrix_get_events( $user,
+               from    => $user->eventstream_token,
+               timeout => 500,
+               limit   => 5,
+            )->on_done( sub {
+               my ( $body ) = @_;
+               my ( $expected_id );
+               my ( @events ) = @{ $body->{chunk} };
+
+               $user->eventstream_token = $body->{end};
+
+               log_if_fail "Events", @events;
+
+               foreach my $event ( @events ) {
+                  if ( $event->{type} eq 'm.room.message' ) {
+                     $expected_id = shift @expected_event_ids;
+
+                     assert_eq( $event->{event_id}, $expected_id,
+                        'Unexpected or out of order event'
+                     );
+                  }
+               }
+            })
+         }, foreach => [ 0 .. 10 ], while => sub {
+            @expected_event_ids > 0
+         } )
+      })->then( sub {
+         @expected_event_ids == 0 or die "Did not see all events.";
+
+         Future->done(1);
+      });
    };
