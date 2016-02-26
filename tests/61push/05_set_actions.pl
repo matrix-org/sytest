3df836177fd37f3320ca0aa5e30f735bcd73c550
Mark Haines <mark.haines@matrix.org>
2016-02-26 12:05:47 +0000

    Check that modifying the actions of unknown rules 404s

--- tests/61push/05_set_actions.pl
+++ tests/61push/05_set_actions.pl
@@ -1,14 +1,14 @@
 use Future::Utils qw( repeat );
 
 sub check_change_action {
-   my ( $user, $kind, $rule_id, $actions ) = @_;
+   my ( $user, $scope, $kind, $rule_id, $actions ) = @_;
 
-   matrix_set_push_rule_actions( $user, "global", $kind, $rule_id, $actions )
+   matrix_set_push_rule_actions( $user, $scope, $kind, $rule_id, $actions )
    ->then( sub {
       # Check that the actions match.
       do_request_json_for( $user,
          method  => "GET",
-         uri     => "/r0/pushrules/global/$kind/$rule_id/actions",
+         uri     => "/r0/pushrules/$scope/$kind/$rule_id/actions",
       )->on_done( sub {
          my ( $body ) = @_;
 
@@ -41,7 +41,48 @@ test "Can change the actions of default rules",
 
             my ( $kind, $rule_id ) = @$to_check;
 
-            check_change_action($user, $kind, $rule_id, $actions);
+            check_change_action( $user, "global", $kind, $rule_id, $actions );
          } foreach => \@to_check;
       });
    };
+
+
+test "Changing the actions of an unknown default rule fails with 404",
+   requires => [ local_user_fixture() ],
+
+   check => sub  {
+      my ( $user ) = @_;
+      matrix_set_push_rule_actions(
+         $user, "global", "override", ".not.a.default.rule", [
+            "notify"
+         ],
+      )->main::expect_http_404;
+   };
+
+
+test "Can change the actions of a user specified rule",
+   requires => [ local_user_fixture() ],
+
+   check => sub {
+      my ( $user ) = @_;
+
+      matrix_add_push_rule( $user, "global", "sender", '@bob:example.com', {
+         actions => [ "notify" ]
+      })->then( sub {
+         check_change_action( $user, "global", "sender", '@bob:example.com', [
+            "dont_notify",
+         ]);
+      });
+   };
+
+
+test "Changing the actions of an unknown rule fails with 404",
+   requires => [ local_user_fixture() ],
+
+   check => sub  {
+      my ( $user ) = @_;
+      matrix_set_push_rule_actions(
+         $user, "global", "sender", '@bob:example.com', [ "notify" ]
+      )->main::expect_http_404;
+   };
+
