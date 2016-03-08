6e788b295265376f6a25d3002fe419db4f23fa22
Richard van der Hoff <richard@matrix.org>
2016-03-08 23:29:53 +0000

    Factor out separate jenkins/prep_sytest_for_postgres.sh

--- jenkins/sytest-postgres.sh
+++ jenkins/sytest-postgres.sh
@@ -8,34 +8,6 @@ set -ex
 
 cd "`dirname $0`/.."
 
-if [ -z "$POSTGRES_DB_1" ]; then
-    echo >&2 "Variable POSTGRES_DB_1 not set"
-    exit 1
-fi
-
-if [ -z "$POSTGRES_DB_2" ]; then
-    echo >&2 "Variable POSTGRES_DB_2 not set"
-    exit 1
-fi
-
-mkdir -p "localhost-$(($PORT_BASE + 1))"
-mkdir -p "localhost-$(($PORT_BASE + 2))"
-
-cat > localhost-$(($PORT_BASE + 1))/database.yaml << EOF
-name: psycopg2
-args:
-    database: $POSTGRES_DB_1
-    user: $POSTGRES_USER_1
-    password: $POSTGRES_PASS_1
-    host: $POSTGRES_HOST_1
-EOF
-
-cat > localhost-$(($PORT_BASE + 2))/database.yaml << EOF
-name: psycopg2
-args:
-    database: $POSTGRES_DB_2
-EOF
-
 ./jenkins/prep_synapse.sh
 
 TOX_BIN="`pwd`/synapse/.tox/py27/bin"
