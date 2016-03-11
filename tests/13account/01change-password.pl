1c5bc4bc6c38125004da7fef75a291e76a06b885
David Baker <dave@matrix.org>
2016-03-11 18:01:57 +0000

    Add some tests for changing password

---
+++ tests/13account/01change-password.pl
@@ -0,0 +1,94 @@
+my $password = "my secure password";
+
+
+test "After changing password, can't log in with old password",
+   requires => [ local_user_fixture( password => $password ) ],
+
+   check => sub {
+      my ( $user, ) = @_;
+
+      my $http = $user->http;
+
+      do_request_json_for( $user,
+         method => "POST",
+         uri    => "/r0/account/password",
+         content => {
+            auth => {
+               type => "m.login.password",
+               user     => $user->user_id,
+               password => $password,
+            },
+            new_password => "my new password",
+         },
+      )->then( sub {
+         $http->do_request_json(
+            method => "POST",
+            uri    => "/r0/login",
+
+            content => {
+               type     => "m.login.password",
+               user     => $user->user_id,
+               password => $password,
+            }
+         )->main::expect_http_4xx;
+      }
+      )->then_done(1);
+   };
+
+test "After changing password, can log in with new password",
+   requires => [ local_user_fixture( password => $password ) ],
+
+   check => sub {
+      my ( $user, ) = @_;
+
+      my $http = $user->http;
+
+      do_request_json_for( $user,
+         method => "POST",
+         uri    => "/r0/account/password",
+         content => {
+            auth => {
+               type => "m.login.password",
+               user     => $user->user_id,
+               password => $password,
+            },
+            new_password => "my new password",
+         },
+      )->then( sub {
+         $http->do_request_json(
+            method => "POST",
+            uri    => "/r0/login",
+
+            content => {
+               type     => "m.login.password",
+               user     => $user->user_id,
+               password => "my new password",
+            }
+         );
+      }
+      )->then_done(1);
+   };
+
+test "After changing password, existing session still works",
+   requires => [ local_user_fixture( password => $password ) ],
+
+   check => sub {
+      my ( $user, ) = @_;
+
+      my $http = $user->http;
+
+      do_request_json_for( $user,
+         method => "POST",
+         uri    => "/r0/account/password",
+         content => {
+            auth => {
+               type => "m.login.password",
+               user     => $user->user_id,
+               password => $password,
+            },
+            new_password => "my new password",
+         },
+      )->then( sub {
+         matrix_sync( $user );
+      })->then_done(1);
+   };
