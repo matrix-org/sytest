3567ada5234a6400eb80bb9fcbfec81addbd8109
David Baker <dave@matrix.org>
2016-03-14 10:50:26 +0000

    Add tests that pushers are deleted at appropriate times on password change

--- tests/14account/01change-password.pl
+++ tests/14account/01change-password.pl
@@ -120,4 +120,101 @@ test "After changing password, a different session no longer works",
       })->then_done(1);
    };
 
-# TODO: Also possibly test that pushers are deleted iff they were created with different access token
+test "Pushers created with a different access token are deleted on password change",
+   requires => [ local_user_fixture( password => $password ) ],
+
+   check => sub {
+      my ( $user, ) = @_;
+
+      my $other_login;
+
+      matrix_login_again_with_user( $user )->then( sub {
+         ( $other_login ) = @_;
+
+         do_request_json_for( $other_login,
+            method  => "POST",
+            uri     => "/r0/pushers/set",
+            content => {
+               profile_tag         => "tag",
+               kind                => "http",
+               app_id              => "sytest",
+               app_display_name    => "sytest_display_name",
+               device_display_name => "device_display_name",
+               pushkey             => "a_push_key",
+               lang                => "en",
+               data                => {
+                  url => "https://dummy.url/is/dummy",
+               },
+            },
+         );
+      })->then( sub {
+         do_request_json_for( $user,
+            method => "POST",
+            uri    => "/r0/account/password",
+            content => {
+               auth => {
+                  type => "m.login.password",
+                  user     => $user->user_id,
+                  password => $password,
+               },
+               new_password => "my new password",
+            });
+      })->then( sub {
+         do_request_json_for( $user,
+            method  => "POST",
+            uri     => "/r0/pushers/set",
+            content => {
+               kind    => JSON::null,
+               app_id  => "sytest",
+               pushkey => "a_push_key",
+            },
+         )->main::expect_http_404;
+      })->then_done(1);
+   };
+
+test "Pushers created with a the same access token are not deleted on password change",
+   requires => [ local_user_fixture( password => $password ) ],
+
+   check => sub {
+      my ( $user, ) = @_;
+
+      do_request_json_for( $user,
+         method  => "POST",
+         uri     => "/r0/pushers/set",
+         content => {
+            profile_tag         => "tag",
+            kind                => "http",
+            app_id              => "sytest",
+            app_display_name    => "sytest_display_name",
+            device_display_name => "device_display_name",
+            pushkey             => "a_push_key",
+            lang                => "en",
+            data                => {
+               url => "https://dummy.url/is/dummy",
+            },
+         },
+      )->then( sub {
+         do_request_json_for( $user,
+            method => "POST",
+            uri    => "/r0/account/password",
+            content => {
+               auth => {
+                  type => "m.login.password",
+                  user     => $user->user_id,
+                  password => $password,
+               },
+               new_password => "my new password",
+            });
+      })->then( sub {
+         do_request_json_for( $user,
+            method  => "POST",
+            uri     => "/r0/pushers/set",
+            content => {
+               kind    => JSON::null,
+               app_id  => "sytest",
+               pushkey => "a_push_key",
+            },
+         );
+      })->then_done(1);
+   };
+
