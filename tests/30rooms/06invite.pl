f11bf865df98e30b72cffd91f5c7b8b885566e16
Erik Johnston <erikj@matrix.org>
2016-03-15 13:33:15 +0000

    When testing invite rejections, test that invitee no longer gets the invite down /sync

--- tests/30rooms/06invite.pl
+++ tests/30rooms/06invite.pl
@@ -142,6 +142,14 @@ sub invited_user_can_reject_invite
          die "Expected membership to be 'leave'";
 
       Future->done(1);
+   })->then( sub {
+      matrix_sync( $invitee )
+   })->then( sub {
+      my ( $body ) = @_;
+
+      assert_json_object( $body->{rooms}{invite} );
+      keys %{ $body->{rooms}{invite} } and die "Expected empty dictionary";
+      Future->done(1);
    });
 }
 
@@ -173,6 +181,14 @@ sub invited_user_can_reject_invite_for_empty_room
    })
    ->then( sub {
       matrix_leave_room( $invitee, $room_id )
+   })->then( sub {
+      matrix_sync( $invitee )
+   })->then( sub {
+      my ( $body ) = @_;
+
+      assert_json_object( $body->{rooms}{invite} );
+      keys %{ $body->{rooms}{invite} } and die "Expected empty dictionary";
+      Future->done(1);
    });
 }
 
