a78a1dab226a6101a10604ebc0ddb45113d768ad
Erik Johnston <erikj@matrix.org>
2016-03-17 14:00:11 +0000

    Deleting aliases should update room

--- tests/30rooms/05aliases.pl
+++ tests/30rooms/05aliases.pl
@@ -147,3 +147,185 @@ test "Users can't delete other's aliases",
          )->main::expect_http_403;
       })
    };
+
+
+test "Can delete canonical alias",
+   requires => [ local_user_fixture( with_events => 0 ), room_alias_fixture(),
+                 qw( can_create_room_alias )],
+
+   do => sub {
+      my ( $creator, $room_alias ) = @_;
+      my $server_name = $creator->http->server_name;
+      my $room_id;
+
+      matrix_create_and_join_room( [ $creator ] )
+      ->then( sub {
+         ( $room_id ) = @_;
+
+         do_request_json_for( $creator,
+            method => "PUT",
+            uri    => "/r0/directory/room/$room_alias",
+
+            content => { room_id => $room_id },
+         )
+      })->then( sub {
+         matrix_put_room_state( $creator, $room_id,
+            type => "m.room.canonical_alias",
+            content => { alias => $room_alias }
+         )
+      })->then( sub {
+         matrix_get_room_state( $creator, $room_id,
+            type      => "m.room.aliases",
+            state_key => $server_name,
+         )
+      })->then( sub {
+         my ( $body ) = @_;
+
+         assert_json_keys( $body, qw( aliases ) );
+         assert_json_list( my $aliases = $body->{aliases} );
+
+         $room_alias ~~ @$aliases or die "Expected alias to be in list";
+
+         do_request_json_for( $creator,
+           method => "DELETE",
+           uri    => "/r0/directory/room/$room_alias",
+
+           content => {},
+         )
+      })->then( sub {
+         matrix_get_room_state( $creator, $room_id,
+            type      => "m.room.aliases",
+            state_key => $server_name,
+         )
+      })->then( sub {
+         my ( $body ) = @_;
+
+         assert_json_keys( $body, qw( aliases ) );
+         assert_json_list( my $aliases = $body->{aliases} );
+
+         not $room_alias ~~ @$aliases or die "Expected alias to not be in list";
+
+         matrix_get_room_state( $creator, $room_id,
+            type      => "m.room.canonical_alias",
+         )
+      })->then( sub {
+         my ( $body ) = @_;
+
+         not defined $body->{alias} or die "Expected canonical alias to be empty";
+
+         Future->done( 1 );
+      })
+   };
+
+test "Alias creators can delete alias with no ops",
+   requires => [ local_user_fixtures( 2 ), room_alias_fixture(), qw( can_create_room_alias )],
+
+   do => sub {
+      my ( $creator, $other_user, $room_alias ) = @_;
+      my $server_name = $creator->http->server_name;
+      my $room_id;
+
+      matrix_create_and_join_room( [ $creator, $other_user ] )
+      ->then( sub {
+         ( $room_id ) = @_;
+
+         do_request_json_for( $other_user,
+            method => "PUT",
+            uri    => "/r0/directory/room/$room_alias",
+
+            content => { room_id => $room_id },
+         )
+      })->then( sub {
+         matrix_get_room_state( $creator, $room_id,
+            type      => "m.room.aliases",
+            state_key => $server_name,
+         )
+      })->then( sub {
+         my ( $body ) = @_;
+
+         assert_json_keys( $body, qw( aliases ) );
+         assert_json_list( my $aliases = $body->{aliases} );
+
+         $room_alias ~~ @$aliases or die "Expected alias to be in list";
+
+         do_request_json_for( $other_user,
+           method => "DELETE",
+           uri    => "/r0/directory/room/$room_alias",
+
+           content => {},
+         )
+      })->then( sub {
+         matrix_get_room_state( $creator, $room_id,
+            type      => "m.room.aliases",
+            state_key => $server_name,
+         )
+      })->then( sub {
+         my ( $body ) = @_;
+
+         assert_json_keys( $body, qw( aliases ) );
+         assert_json_list( my $aliases = $body->{aliases} );
+
+         not $room_alias ~~ @$aliases or die "Expected alias to not be in list";
+
+         Future->done(1);
+      })
+   };
+
+test "Alias creators can delete canonical alias with no ops",
+   requires => [ local_user_fixtures( 2 ), room_alias_fixture(), qw( can_create_room_alias )],
+
+   do => sub {
+      my ( $creator, $other_user, $room_alias ) = @_;
+      my $server_name = $creator->http->server_name;
+      my $room_id;
+
+      matrix_create_and_join_room( [ $creator, $other_user ] )
+      ->then( sub {
+         ( $room_id ) = @_;
+
+         do_request_json_for( $other_user,
+            method => "PUT",
+            uri    => "/r0/directory/room/$room_alias",
+
+            content => { room_id => $room_id },
+         )
+      })->then( sub {
+         matrix_put_room_state( $creator, $room_id,
+            type => "m.room.canonical_alias",
+            content => { alias => $room_alias }
+         )
+      })->then( sub {
+         matrix_get_room_state( $creator, $room_id,
+            type      => "m.room.aliases",
+            state_key => $server_name,
+         )
+      })->then( sub {
+         my ( $body ) = @_;
+
+         assert_json_keys( $body, qw( aliases ) );
+         assert_json_list( my $aliases = $body->{aliases} );
+
+         $room_alias ~~ @$aliases or die "Expected alias to be in list";
+
+         do_request_json_for( $other_user,
+           method => "DELETE",
+           uri    => "/r0/directory/room/$room_alias",
+
+           content => {},
+         )
+      })->then( sub {
+         matrix_get_room_state( $creator, $room_id,
+            type      => "m.room.aliases",
+            state_key => $server_name,
+         )
+      })->then( sub {
+         my ( $body ) = @_;
+
+         assert_json_keys( $body, qw( aliases ) );
+         assert_json_list( my $aliases = $body->{aliases} );
+
+         not $room_alias ~~ @$aliases or die "Expected alias to not be in list";
+
+         Future->done(1);
+      })
+   };
