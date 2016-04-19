c7f21e916a1fb97ec9fac89db544910a4590366a
Erik Johnston <erikj@matrix.org>
2016-04-19 17:44:12 +0100

    Reuse venv when running sytests

--- jenkins/prep_synapse.sh
+++ jenkins/prep_synapse.sh
@@ -12,19 +12,25 @@ if [ ! -d .synapse-base ]; then
 else
   (cd .synapse-base; git fetch -p)
 fi
+
 rm -rf synapse
 git clone .synapse-base synapse --shared
 
 : ${GIT_BRANCH:="origin/$(git rev-parse --abbrev-ref HEAD)"}
 
+mkdir -p .tox
 
 cd synapse
 
 # check out the relevant branch of synapse
 git checkout "${GIT_BRANCH}" || (
     echo >&2 "No ref ${GIT_BRANCH} found, falling back to develop"
-    git checkout develop
+    git checkout "origin/develop"
 )
+git clean -df .
+
+ln -s ../.tox .tox
 
 # set up the virtualenv
 tox -e py27 --notest -v
+./.tox/py27/bin/pip install lxml
