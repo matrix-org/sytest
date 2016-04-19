844fedb4890ac3baf19a544ae2f70edf9cd3b3bf
Mark Haines <mjark@negativecurvature.net>
2016-04-19 18:37:10 +0100

    whitespace

--- tests/61push/08_rejected_pushers.pl
+++ tests/61push/08_rejected_pushers.pl
@@ -55,7 +55,7 @@ multi_test "Test that rejected pushers are removed.",
 
          matrix_invite_user_to_room( $alice, $bob, $room_id );
       })->then( sub {
-         matrix_join_room( $bob, $room_id);
+         matrix_join_room( $bob, $room_id );
       })->then( sub {
          matrix_send_room_text_message(
             $bob, $room_id, body => "message"
