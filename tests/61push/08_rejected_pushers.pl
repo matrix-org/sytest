4ddc775b90d3d3104e33d410d03a84ef3865d669
Mark Haines <mjark@negativecurvature.net>
2016-04-19 18:34:00 +0100

    Send a second message to increase the chance that the previous messages have been processed

--- tests/61push/08_rejected_pushers.pl
+++ tests/61push/08_rejected_pushers.pl
@@ -21,6 +21,7 @@ sub create_pusher
 sub wait_for_push
 {
    my ( $path, $response ) = @_;
+
    await_http_request( $path, sub {
      my ( $request ) = @_;
      my $body = $request->body_from_json;
@@ -57,12 +58,14 @@ multi_test "Test that rejected pushers are removed.",
       })->then( sub {
          matrix_send_room_text_message(
             $bob, $room_id, body => "message"
-         )->then( sub {
-            my ( $event_id ) = @_;
-            matrix_advance_room_receipt( $alice, $room_id,
-                "m.read" => $event_id
-            );
-         });
+         );
+      })->then( sub {
+         my ( $event_id ) = @_;
+
+         # Set a read receipt so that we pushed for the subsequent messages.
+         matrix_advance_room_receipt( $alice, $room_id,
+            "m.read" => $event_id
+         );
       })->then( sub {
          create_pusher( $alice, "sytest", "key_1", "$url/1" )
             ->SyTest::pass_on_done( "Alice's pusher 1 created" );
@@ -86,8 +89,16 @@ multi_test "Test that rejected pushers are removed.",
             wait_for_push( "/alice_push/1",  { rejected => [ "key_1" ] } ),
             wait_for_push( "/alice_push/2" ),
             matrix_send_room_text_message( $bob, $room_id, body => "message" )
-               ->SyTest::pass_on_done( "Message Sent" ),
-         );
+               ->SyTest::pass_on_done( "Message 1 Sent" ),
+         )->SyTest::pass_on_done( "Message 1 Pushed" );
+      })->then( sub {
+         # Send another push message to increase the chance that previous
+         # messages have been processed.
+         Future->needs_all(
+            wait_for_push( "/alice_push/2" ),
+            matrix_send_room_text_message( $bob, $room_id, body => "message" )
+               ->SyTest::pass_on_done( "Message 2 Sent" ),
+         )->SyTest::pass_on_done( "Message 2 Pushed" );
       })->then( sub {
          do_request_json_for( $alice,
               method  => "GET",
