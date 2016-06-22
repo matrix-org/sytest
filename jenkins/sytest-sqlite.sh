6fbd6b80cc8594f861d2380b05d5c8c67a81c863
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-06-22 17:13:30 +0100

    Fix jenkins helper scripts for --port-range argument

--- jenkins/sytest-sqlite.sh
+++ jenkins/sytest-sqlite.sh
@@ -5,10 +5,11 @@
 set -ex
 
 : ${PORT_BASE=8000}
+: ${PORT_COUNT=20}
 
 cd "`dirname $0`/.."
 
 ./jenkins/prep_synapse.sh
 
 TOX_BIN="`pwd`/synapse/.tox/py27/bin"
-./jenkins/install_and_run.sh --python="$TOX_BIN/python" --port-base ${PORT_BASE}
+./jenkins/install_and_run.sh --python="$TOX_BIN/python" --port-range ${PORT_BASE}:$((PORT_BASE+PORT_COUNT-1))
