8d7460790bee977eafea6fc4688493601b8efbf0
Erik Johnston <erikj@matrix.org>
2016-07-07 11:19:47 +0100

    Add backfill test to /purge_history

--- tests/48admin.pl
+++ tests/48admin.pl
@@ -1,4 +1,4 @@
-use Future::Utils qw( try_repeat );
+use Future::Utils qw( repeat );
 
 test "/whois",
    requires => [ $main::API_CLIENTS[0] ],
@@ -51,7 +51,7 @@ test "/purge_history",
          type    => "m.room.name",
          content => { name => "A room name" },
       )->then( sub {
-         try_repeat( sub {
+         repeat( sub {
             my $msgnum = $_[0];
 
             matrix_send_room_text_message_synced( $user, $room_id,
@@ -91,6 +91,7 @@ test "/purge_history",
             $_->{type} ne "m.room.message" || $_->{event_id} eq $last_event_id
          } @{ $room->{timeline}{events} } or die "Expected no message events";
 
+         # Ensure we still see the state.
          foreach my $expected_type( qw(
             m.room.create
             m.room.member
@@ -103,4 +104,122 @@ test "/purge_history",
 
          Future->done( 1 );
       })
+   };
+
+test "Can backfill purged history",
+   requires => [ local_admin_fixture(), local_user_and_room_fixtures(),
+                 remote_user_fixture(), qw( can_paginate_room_remotely ) ],
+
+   do => sub {
+      my ( $admin, $user, $room_id, $remote_user ) = @_;
+
+      my @event_ids;
+      my $last_event_id;
+
+      matrix_invite_user_to_room( $user, $remote_user, $room_id )
+      ->then( sub {
+         matrix_join_room( $remote_user, $room_id )
+      })->then( sub {
+         matrix_put_room_state( $user, $room_id,
+            type    => "m.room.name",
+            content => { name => "A room name" },
+         )
+      })->then( sub {
+         matrix_sync( $remote_user )
+      })->then( sub {
+         repeat( sub {
+            my $msgnum = $_[0];
+
+            matrix_send_room_text_message_synced( $user, $room_id,
+               body => "Message $msgnum",
+            )->on_done( sub { push @event_ids, @_[0]; } )
+         }, foreach => [ 0 .. 4 ])
+      })->then( sub {
+         repeat( sub {
+            my $msgnum = $_[0];
+
+            matrix_send_room_text_message_synced( $remote_user, $room_id,
+               body => "Message $msgnum",
+            )->on_done( sub { push @event_ids, @_[0]; } )
+         }, foreach => [ 5 .. 9 ])
+      })->then( sub {
+         ( $last_event_id ) = @_;
+
+         # Wait until the remote server has received the last message, so that
+         # we know we can later backfill from the remote to get all the events.
+         repeat( sub {
+            matrix_sync_again( $remote_user )
+            ->on_done( sub {
+               my ( $body ) = @_;
+
+               log_if_fail "Remote sync", $body;
+
+               Future->done( any {
+                  $_->{event_id} eq $last_event_id
+               } @{ $body->{rooms}{join}{$room_id}{timeline}{events} } )
+            })
+         }, until => sub {
+            $_[0]->failure or $_[0]->get
+         })
+      })->then( sub {
+         do_request_json_for( $admin,
+            method  => "POST",
+            uri     => "/r0/admin/purge_history/$room_id/$last_event_id",
+            content => {}
+         )
+      })->then( sub {
+         matrix_sync( $user )
+      })->then( sub {
+         my ( $body ) = @_;
+
+         assert_json_keys( $body->{rooms}{join}, $room_id );
+         my $room =  $body->{rooms}{join}{$room_id};
+
+         log_if_fail( "Room timeline", $room->{timeline}{events} );
+
+         # The only message event should be the last one.
+         all {
+            $_->{type} ne "m.room.message" || $_->{event_id} eq $last_event_id
+         } @{ $room->{timeline}{events} } or die "Expected no message events";
+
+         # Ensure we still see the state.
+         foreach my $expected_type( qw(
+            m.room.create
+            m.room.member
+            m.room.power_levels
+            m.room.name
+         ) ) {
+            any { $_->{type} eq $expected_type } @{ $room->{state}{events} }
+               or die "Expected state event of type $expected_type";
+         }
+
+         my $prev_batch = $room->{timeline}{prev_batch};
+
+         my @missing_event_ids = grep { $_ ne $last_event_id } @event_ids;
+
+         # Keep paginating untill we see all the old messages.
+         repeat( sub {
+            log_if_fail "prev_batch", $prev_batch;
+            matrix_get_room_messages( $user, $room_id,
+               limit => 20,
+               from => $prev_batch,
+            )->on_done( sub {
+               my ( $body ) = @_;
+
+               log_if_fail( "Pagination result", $body );
+
+               $prev_batch ne $body->{end} or die "Pagination token did not change";
+
+               $prev_batch = $body->{end};
+
+               foreach my $event ( @{ $body->{chunk} } ) {
+                  @missing_event_ids = grep {
+                     $_ ne $event->{event_id}
+                  } @missing_event_ids;
+               }
+
+               log_if_fail "Missing", \@missing_event_ids;
+            })
+         }, while => sub { scalar @missing_event_ids > 0 });
+      });
    };
