d413d10f96e6842cbdb5812d93ed02984a307f0b
Mark Haines <mark.haines@matrix.org>
2016-07-21 11:30:55 +0100

    Only kill one process at a time

--- jenkins/kill_old_listeners.sh
+++ jenkins/kill_old_listeners.sh
@@ -4,5 +4,5 @@
 # that are holding onto one of the ports we want to use.
 : ${PORT_COUNT=20}
 for port in $(seq $PORT_BASE $((PORT_BASE+PORT_COUNT-1))); do
-    lsof -i TCP:$port | grep LISTEN | awk '{print $2}' | xargs --no-run-if-empty kill -9
+    lsof -i TCP:$port | grep LISTEN | awk '{print $2}' | xargs -n 1 --no-run-if-empty kill -9
 done
