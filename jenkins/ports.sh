35e30eeea498c0f1803636de7bee874f40bee9af
Mark Haines <mark.haines@matrix.org>
2016-08-02 16:08:52 +0100

    Include the BIND_HOST in ports.sh

--- jenkins/ports.sh
+++ jenkins/ports.sh
@@ -3,7 +3,7 @@
 # Prints the ports and databases used by jenkins.
 
 ssh jenkins@adc-prd-matrix-001 bash <<EOF
-grep -E 'PORT_BASE|POSTGRES_DB_1|POSTGRES_DB_2' /var/lib/jenkins/jobs/*/config.xml \
+grep -E 'PORT_BASE|POSTGRES_DB_1|POSTGRES_DB_2|BIND_HOST|PORT_COUNT' /var/lib/jenkins/jobs/*/config.xml \
     | cut -c23- \
     | sed 's|/config.xml:\( *<command>\)*export||;s|=| |'
 EOF
