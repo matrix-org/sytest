48fb35ffaf85db4b09814fd4933edd041b6f5fd8
Mark Haines <mark.haines@matrix.org>
2016-08-02 16:13:41 +0100

    Increase the port range to 100

--- jenkins/kill_old_listeners.sh
+++ jenkins/kill_old_listeners.sh
@@ -2,7 +2,7 @@
 
 # Kill any stray processes that left over from a previous test run
 # that are holding onto one of the ports we want to use.
-: ${PORT_COUNT=20}
+: ${PORT_COUNT=100}
 for port in $(seq $PORT_BASE $((PORT_BASE+PORT_COUNT-1))); do
     lsof -i TCP:$port | grep LISTEN | awk '{print $2}' | xargs -n 1 --no-run-if-empty kill -9
 done
