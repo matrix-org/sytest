612b1deaa9b006ec69e7cd31272d06dc3cc4788f
Mark Haines <mjark@negativecurvature.net>
2016-08-03 14:00:41 +0100

    Factor out some of the code shared between sytest scripts (#277)
    * Factor out some of the code shared between sytest scripts
    
    * Increase the default port range
    
    * inline prep_synapse since it is only two lines
    
    * Port range is inclusive
    
    * Add quotes, remove redundant clean, add comments
    
    * Grammar

--- jenkins/kill_old_listeners.sh
+++ jenkins/kill_old_listeners.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-set -x
+echo "Killing processes using $PORT_COUNT ports starting from $BIND_HOST:$PORT_BASE"
 
 # Kill any stray processes that left over from a previous test run
 # that are holding onto one of the ports we want to use.
-: ${PORT_COUNT=100}
 for port in $(seq $PORT_BASE $((PORT_BASE+PORT_COUNT-1))); do
-    lsof -i TCP@${BIND_HOST:-localhost}:$port | grep LISTEN | awk '{print $2}' | xargs -n 1 --no-run-if-empty kill -9
+    lsof -i TCP@${BIND_HOST}:$port | grep LISTEN | awk '{print $2}' | xargs -n 1 --no-run-if-empty kill -9
 done
--- jenkins/sytest-sqlite.sh
+++ jenkins/sytest-sqlite.sh
@@ -4,12 +4,8 @@
 
 set -ex
 
-: ${PORT_BASE=8000}
-: ${PORT_COUNT=100}
-
 cd "`dirname $0`/.."
 
-./jenkins/prep_synapse.sh
-
-TOX_BIN="`pwd`/synapse/.tox/py27/bin"
-./jenkins/install_and_run.sh --python="$TOX_BIN/python" --port-range ${PORT_BASE}:$((PORT_BASE+PORT_COUNT-1))
+./jenkins/clone.sh synapse https://github.com/matrix-org/synapse.git
+./synapse/jenkins/prepare_synapse.sh
+./jenkins/install_and_run.sh
