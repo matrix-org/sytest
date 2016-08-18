78e7b50aad3d40596c6ed80aba46e51afd1f6f49
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-08-18 16:13:42 +0100

    Also test 3PL lookup

--- tests/60app-services/05lookup3pe.pl
+++ tests/60app-services/05lookup3pe.pl
@@ -51,3 +51,55 @@ test "HS will proxy request for 3PU mapping",
          }),
       )
    };
+
+test "HS will proxy request for 3PL mapping",
+   requires => [ local_user_fixture(), $main::APPSERV[0] ],
+
+   do => sub {
+      my ( $user, $appserv ) = @_;
+
+      Future->needs_all(
+         $appserv->await_http_request( "/3pl/protocol", sub { 1 } )->then( sub {
+            my ( $request ) = @_;
+
+            assert_deeply_eq( { $request->query_form },
+               {
+                  field3 => "THREE",
+               },
+            'fields in received AS request' );
+
+            $request->respond_json( [
+               {
+                  protocol => "protocol",
+                  fields   => { field3 => "result" },
+                  alias    => '#remote-room:bridged.example.com',
+               }
+            ] );
+
+            Future->done(1);
+         }),
+
+         do_request_json_for( $user,
+            method => "GET",
+            uri    => "/unstable/3pl/protocol",
+
+            params => {
+               field3 => "THREE",
+            }
+         )->then( sub {
+            my ( $body ) = @_;
+
+            log_if_fail "Lookup result", $body;
+
+            assert_deeply_eq( $body, [
+               {
+                  protocol => "protocol",
+                  fields   => { field3 => "result" },
+                  alias    => '#remote-room:bridged.example.com',
+               }
+            ], '3PL lookup result' );
+
+            Future->done(1);
+         }),
+      )
+   };
