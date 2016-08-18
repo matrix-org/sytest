921a50d2328ca2a05aef264660813077cc63b213
Mark Haines <mark.haines@matrix.org>
2016-08-18 09:37:10 +0100

    optionally send media requests to a separate worker process

--- run-tests.pl
+++ run-tests.pl
@@ -53,6 +53,7 @@ our %SYNAPSE_ARGS = (
 
    synchrotron       => 0,
    federation_reader => 0,
+   media_repository  => 0,
 );
 
 our $WANT_TLS = 1;  # This is shared with the test scripts
@@ -91,6 +92,8 @@ GetOptions(
    'synchrotron+' => \$SYNAPSE_ARGS{synchrotron},
 
    'federation-reader+' => \$SYNAPSE_ARGS{federation_reader},
+
+   'media-repository+' => \$SYNAPSE_ARGS{media_repository},
 
    'bind-host=s' => \$BIND_HOST,
 
