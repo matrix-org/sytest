68852d98ac44b21368451e648bb3aac57460c809
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-09-08 17:14:40 +0100

    Don't construct ASInfo structures in a loop, because we're about to want differing ones

--- tests/02as-info.pl
+++ tests/02as-info.pl
@@ -12,25 +12,27 @@ struct ASInfo => [qw( localpart user_id as2hs_token hs2as_token path id
 
 my $n_appservers = 1;
 
+# The actual infos
+my @as_info = (
+   # user_id will be filled in later once the homeserver is started
+
+   ASInfo(
+      localpart     => "as-user-1",
+      user_id       => undef,
+      as2hs_token   => gen_token( 32 ),
+      hs2as_token   => gen_token( 32 ),
+      path          => "/appservs/1",
+      id            => "AS-1",
+      user_regexes  => [ '@astest-.*' ],
+      alias_regexes => [ '#astest-.*' ],
+      protocols     => [ 'ymca' ],
+   ),
+);
+
 our @AS_INFO = map {
    my $idx = $_;
 
    fixture(
-      setup => sub {
-         my $localpart = "as-user-$idx";
-
-         Future->done( ASInfo(
-            localpart     => $localpart,
-            user_id       => undef, # will be filled in later when we know what
-                                    # the homeserver location actually is
-            as2hs_token   => gen_token( 32 ),
-            hs2as_token   => gen_token( 32 ),
-            path          => "/appservs/$idx",
-            id            => "AS-$idx",
-            user_regexes  => [ '@astest-.*' ],
-            alias_regexes => [ '#astest-.*' ],
-            protocols     => [ 'ymca' ],
-         ));
-      },
+      setup => sub { Future->done( $as_info[$idx] ) },
    );
-} 1 .. $n_appservers;
+} 0 .. $#as_info;
