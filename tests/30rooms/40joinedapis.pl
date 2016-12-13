30ff54cb646641653a1b7164d22cfde77d2d6750
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2016-12-13 16:18:42 +0000

    Rename $user and $room variables to better convey what they do

--- tests/30rooms/40joinedapis.pl
+++ tests/30rooms/40joinedapis.pl
@@ -2,32 +2,32 @@ test "/joined_rooms returns only joined rooms",
    requires => [ local_user_fixture(), local_user_fixture(), ],
 
    do => sub {
-      my ( $user1, $user2 ) = @_;
+      my ( $user, $inviter ) = @_;
 
-      my ( $room1, $room2, $room3 );
+      # Create three rooms; one joined, one joined-then-left, one only invited
+      my ( $room_joined, $room_left, $room_invited );
 
-      matrix_create_room( $user1 )->then( sub {
-         ( $room1 ) = @_;
+      Future->needs_all(
+         matrix_create_room( $user )->on_done( sub {
+            ( $room_joined ) = @_;
+            log_if_fail "room joined", $room_joined;
+         }),
 
-         log_if_fail "room 1", $room1;
+         matrix_create_room( $user )->then( sub {
+            ( $room_left ) = @_;
+            log_if_fail "room left", $room_left;
 
-         matrix_create_room( $user1 );
-      })->then( sub {
-         ( $room2 ) = @_;
+            matrix_leave_room( $user, $room_left );
+         }),
 
-         log_if_fail "room 2", $room2;
+         matrix_create_room( $inviter )->then( sub {
+            ( $room_invited ) = @_;
+            log_if_fail "room invited", $room_invited;
 
-         matrix_leave_room( $user1, $room2 );
-      })->then( sub {
-         matrix_create_room( $user2 );
-      })->then( sub {
-         ( $room3 ) = @_;
-
-         log_if_fail "room 3", $room3;
-
-         matrix_invite_user_to_room( $user2, $user1, $room3 );
-      })->then( sub {
-         do_request_json_for( $user1,
+            matrix_invite_user_to_room( $inviter, $user, $room_invited );
+         }),
+      )->then( sub {
+         do_request_json_for( $user,
             method => "GET",
             uri => "/unstable/joined_rooms",
          )
@@ -37,12 +37,14 @@ test "/joined_rooms returns only joined rooms",
          log_if_fail "Body", $body;
 
          assert_json_keys( $body, qw( joined_rooms ) );
-         assert_json_list( my $joined_rooms = $body->{joined_rooms});
+         assert_json_list( my $joined_rooms = $body->{joined_rooms} );
 
-         assert_eq( scalar @$joined_rooms, 1, "expected only 1 room" );
-         assert_eq( $joined_rooms->[0], $room1, "wrong room returned" );
+         # Only $room_joined should appear
 
-         Future->done( 1 );
+         assert_eq( scalar @$joined_rooms, 1, "1 room returned" );
+         assert_eq( $joined_rooms->[0], $room_joined, "joined_rooms[0]" );
+
+         Future->done(1);
       });
    };
 
@@ -51,43 +53,44 @@ test "/joined_members return joined members",
    requires => [ local_user_fixtures( 3 ) ],
 
    do => sub {
-      my ( $user1, $user2, $user3 ) = @_;
+      my ( $user, $user_left, $user_invited ) = @_;
+
+      # Create three users; one joined, one joined-then-left, one only invited
 
       my $room_id;
 
-      my $user_id1 = $user1->user_id;
       my $display_name = "Display Name";
       my $avatar_url = "http://example.com/avatar.png";
 
       Future->needs_all(
-         do_request_json_for( $user1,
+         do_request_json_for( $user,
             method  => "PUT",
-            uri     => "/r0/profile/$user_id1/displayname",
+            uri     => "/r0/profile/:user_id/displayname",
             content => {
                displayname => $display_name,
             },
          ),
-         do_request_json_for( $user1,
+         do_request_json_for( $user,
             method  => "PUT",
-            uri     => "/r0/profile/$user_id1/avatar_url",
+            uri     => "/r0/profile/:user_id/avatar_url",
             content => {
                avatar_url => $avatar_url,
             },
          )
       )->then( sub {
-         matrix_create_room( $user1,
-            invite => [ $user2->user_id, $user3->user_id ],
+         matrix_create_room( $user,
+            invite => [ $user_left->user_id, $user_invited->user_id ],
          )
       })->then( sub {
          ( $room_id ) = @_;
 
          log_if_fail "room", $room_id;
 
-         matrix_join_room( $user2, $room_id )
+         matrix_join_room( $user_left, $room_id )
       })->then( sub {
-         matrix_leave_room( $user2, $room_id )
+         matrix_leave_room( $user_left, $room_id )
       })->then( sub {
-         do_request_json_for( $user1,
+         do_request_json_for( $user,
             method => "GET",
             uri => "/unstable/rooms/$room_id/joined_members",
          )
@@ -98,13 +101,13 @@ test "/joined_members return joined members",
 
          assert_deeply_eq( $body, {
             joined => {
-               $user1->user_id => {
+               $user->user_id => {
                   display_name => $display_name,
                   avatar_url => $avatar_url,
                }
             }
          } );
 
-         Future->done( 1 );
+         Future->done(1);
       })
    };
