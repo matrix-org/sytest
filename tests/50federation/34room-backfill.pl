757ed4f65a2f4adc2c03c9324680d9ae63da1b1e
Erik Johnston <erikj@matrix.org>
2016-12-14 13:41:24 +0000

    Add test to check /backfill returns redacted events

--- tests/50federation/34room-backfill.pl
+++ tests/50federation/34room-backfill.pl
@@ -178,6 +178,78 @@ test "Inbound federation can backfill events",
          # sanity-check it too far
          foreach my $event ( @$events ) {
             assert_json_keys( $event, qw( type event_id ));
+            if ( $event->{type} eq "m.room.message" ) {
+               assert_eq( $event->{content}{msgtype}, "m.text" );
+            }
+         }
+
+         assert_eq( $events->[0]{event_id}, $join_event->{prev_events}[0][0],
+            'event_id of first returned event' );
+
+         Future->done(1);
+      });
+   };
+
+test "Inbound federation can backfill events, and they're redacted",
+   requires => [ $main::OUTBOUND_CLIENT, $main::HOMESERVER_INFO[0],
+                 local_user_and_room_fixtures(),
+                 federation_user_id_fixture() ],
+
+   do => sub {
+      my ( $outbound_client, $info, $creator, $room_id, $user_id ) = @_;
+      my $first_home_server = $info->server_name;
+
+      my $local_server_name = $outbound_client->server_name;
+
+      my $join_event;
+
+      matrix_set_room_history_visibility( $creator, $room_id, "joined" )
+      ->then( sub {
+         # Create some past messages to backfill from
+         repeat {
+            matrix_send_room_text_message( $creator, $room_id,
+               body => "Message $_[0] here",
+            )
+         } foreach => [ 1 .. 10 ]
+      })->then( sub {
+         $outbound_client->join_room(
+            server_name => $first_home_server,
+            room_id     => $room_id,
+            user_id     => $user_id,
+         );
+      })->then( sub {
+         my ( $room ) = @_;
+
+         $join_event = $room->get_current_state_event( "m.room.member", $user_id );
+         log_if_fail "Join event", $join_event;
+
+         $outbound_client->do_request_json(
+            method   => "GET",
+            hostname => $first_home_server,
+            uri      => "/backfill/$room_id/",
+
+            params => {
+               v     => $join_event->{prev_events}[0][0],
+               limit => 100,
+            },
+         );
+      })->then( sub {
+         my ( $body ) = @_;
+         log_if_fail "Backfill response", $body;
+
+         assert_json_keys( $body, qw( origin pdus ));
+
+         assert_eq( $body->{origin}, $first_home_server,
+            'body origin' );
+         assert_json_list( my $events = $body->{pdus} );
+
+         # Each element of @$events ought to look like an event. We won't
+         # sanity-check it too far
+         foreach my $event ( @$events ) {
+            assert_json_keys( $event, qw( type event_id content ));
+            if ( $event->{type} eq "m.room.message" ) {
+               assert_deeply_eq( $event->{content}, {} );
+            }
          }
 
          assert_eq( $events->[0]{event_id}, $join_event->{prev_events}[0][0],
