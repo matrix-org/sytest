4bccf1428fe8e739ca6baf0211fd99a6abf694fe
Mark Haines <mjark@negativecurvature.net>
2017-01-05 16:42:10 +0000

    Check that remote users get told about their ban (#335)

--- tests/30rooms/07ban.pl
+++ tests/30rooms/07ban.pl
@@ -1,3 +1,5 @@
+use Future::Utils qw( try_repeat_until_success );
+
 my $creator_fixture = local_user_fixture();
 
 my $banned_user_fixture = local_user_fixture();
@@ -56,4 +58,56 @@ test "Banned user is kicked and may not rejoin until unbanned",
             content => {},
          );
       })
+   };
+
+
+my $banned_remote_user_fixture = remote_user_fixture();
+
+test "Remote banned user is kicked and may not rejoin until unbanned",
+   requires => [ $creator_fixture, $banned_remote_user_fixture,
+                     magic_room_fixture( requires_users => [ $creator_fixture, $banned_remote_user_fixture ] ),
+                qw( can_ban_room )],
+
+   do => sub {
+      my ( $creator, $banned_user, $room_id ) = @_;
+
+      do_request_json_for( $creator,
+         method => "POST",
+         uri    => "/r0/rooms/$room_id/ban",
+
+         content => { user_id => $banned_user->user_id, reason => "testing" },
+      )->then( sub {
+         matrix_get_room_state( $creator, $room_id,
+            type      => "m.room.member",
+            state_key => $banned_user->user_id,
+         )
+      })->then( sub {
+         my ( $body ) = @_;
+         $body->{membership} eq "ban" or
+            die "Expected banned user membership to be 'ban'";
+
+         try_repeat_until_success( sub {
+            matrix_get_room_state( $banned_user, $room_id,
+               type      => "m.room.member",
+               state_key => $banned_user->user_id,
+            )->main::expect_http_403;
+         })
+      })->then( sub {
+         matrix_join_room( $banned_user, $room_id )
+            ->main::expect_http_403;  # Must be unbanned first
+      })->then( sub {
+         do_request_json_for( $creator,
+            method => "POST",
+            uri    => "/r0/rooms/$room_id/invite",
+
+            content => { user_id => $banned_user->user_id },
+         )->main::expect_http_403;  # Must be unbanned first
+      })->then( sub {
+         do_request_json_for( $creator,
+            method => "POST",
+            uri    => "/r0/rooms/$room_id/kick",
+
+            content => { user_id => $banned_user->user_id },
+         )->main::expect_http_403;  # Must be unbanned first
+      });
    };
