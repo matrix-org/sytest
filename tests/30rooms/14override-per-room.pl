785da4965cbdd21e1ede50c27be85b50999fbc2b
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2017-01-24 18:11:33 +0000

    Also test that newly-joining members can override displayname

--- tests/30rooms/14override-per-room.pl
+++ tests/30rooms/14override-per-room.pl
@@ -29,3 +29,36 @@ test "Room members can override their displayname on a room-specific basis",
          Future->done(1);
       });
    };
+
+test "Room members can join a room with an overridden displayname",
+   bug => "#1382",
+
+   requires => [ local_user_and_room_fixtures(), local_user_fixture() ],
+
+   do => sub {
+      my ( $creator, $room_id, $joiner ) = @_;
+
+      # PUT'ing my membership state should join me
+      matrix_put_room_state( $joiner, $room_id,
+         type      => "m.room.member",
+         state_key => $joiner->user_id,
+         content   => {
+            membership => "join",
+            displayname => "Overridden",
+         },
+      )->then( sub {
+         matrix_get_room_state( $creator, $room_id,
+            type      => "m.room.member",
+            state_key => $joiner->user_id,
+         );
+      })->then( sub {
+         my ( $state ) = @_;
+
+         log_if_fail "State", $state;
+
+         assert_eq( $state->{displayname}, "Overridden",
+            'displayname in my m.room.member event at join time' );
+
+         Future->done(1);
+      });
+   };
