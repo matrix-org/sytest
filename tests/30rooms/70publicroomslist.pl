ba47c5b1ec1839e0023e01467e8a0413ad36b8e9
Erik Johnston <erikj@matrix.org>
2017-05-03 15:41:32 +0100

    Handle eventual consistency in public room tests

--- tests/30rooms/70publicroomslist.pl
+++ tests/30rooms/70publicroomslist.pl
@@ -1,3 +1,6 @@
+use Future::Utils qw( try_repeat_until_success );
+
+
 test "Name/topic keys are correct",
    requires => [ $main::API_CLIENTS[0], local_user_fixture() ],
 
@@ -30,72 +33,74 @@ test "Name/topic keys are correct",
          )
       } keys %rooms )
       ->then( sub {
-         $http->do_request_json(
-            method => "GET",
-            uri    => "/r0/publicRooms",
-         )
-      })->then( sub {
-         my ( $body ) = @_;
+         try_repeat_until_success( sub {
+            $http->do_request_json(
+               method => "GET",
+               uri    => "/r0/publicRooms",
+            )->then( sub {
+               my ( $body ) = @_;
 
-         log_if_fail "publicRooms", $body;
+               log_if_fail "publicRooms", $body;
 
-         assert_json_keys( $body, qw( chunk ));
-         assert_json_list( $body->{chunk} );
+               assert_json_keys( $body, qw( chunk ));
+               assert_json_list( $body->{chunk} );
 
-         my %seen = map {
-            $_ => 0,
-         } keys ( %rooms );
+               my %seen = map {
+                  $_ => 0,
+               } keys ( %rooms );
 
-         foreach my $room ( @{ $body->{chunk} } ) {
-            assert_json_keys( $room,
-               qw( world_readable guest_can_join num_joined_members )
-            );
+               foreach my $room ( @{ $body->{chunk} } ) {
+                  assert_json_keys( $room,
+                     qw( world_readable guest_can_join num_joined_members )
+                  );
 
-            my $name = $room->{name};
-            my $topic = $room->{topic};
-            my $canonical_alias = $room->{canonical_alias};
+                  my $name = $room->{name};
+                  my $topic = $room->{topic};
+                  my $canonical_alias = $room->{canonical_alias};
 
-            my $aliases = $room->{aliases};
-            if( not defined $aliases ) {
-               next;
-            }
-
-            foreach my $alias ( @{$aliases} ) {
-               foreach my $alias_local ( keys %rooms ) {
-                  $alias =~ m/^\Q#$alias_local:\E/ or next;
-
-                  my $room_config = $rooms{$alias_local};
-
-                  log_if_fail "Alias", $alias_local;
-                  log_if_fail "Room", $room;
-
-                  assert_eq( $canonical_alias, $alias, "Incorrect canonical_alias" );
-                  assert_eq( $room->{num_joined_members}, 1, "Incorrect member count" );
-
-                  if( defined $name ) {
-                     assert_eq( $room_config->{name}, $name, 'room name' );
-                  }
-                  else {
-                     defined $room_config->{name} and die "Expected not to find a name";
+                  my $aliases = $room->{aliases};
+                  if( not defined $aliases ) {
+                     next;
                   }
 
-                  if( defined $topic ) {
-                     assert_eq( $room_config->{topic}, $topic, 'room topic' );
-                  }
-                  else {
-                     defined $room_config->{topic} and die "Expected not to find a topic";
-                  }
+                  foreach my $alias ( @{$aliases} ) {
+                     foreach my $alias_local ( keys %rooms ) {
+                        $alias =~ m/^\Q#$alias_local:\E/ or next;
 
-                  $seen{$alias_local} = 1;
+                        my $room_config = $rooms{$alias_local};
+
+                        log_if_fail "Alias", $alias_local;
+                        log_if_fail "Room", $room;
+
+                        assert_eq( $canonical_alias, $alias, "Incorrect canonical_alias" );
+                        assert_eq( $room->{num_joined_members}, 1, "Incorrect member count" );
+
+                        if( defined $name ) {
+                           assert_eq( $room_config->{name}, $name, 'room name' );
+                        }
+                        else {
+                           defined $room_config->{name} and die "Expected not to find a name";
+                        }
+
+                        if( defined $topic ) {
+                           assert_eq( $room_config->{topic}, $topic, 'room topic' );
+                        }
+                        else {
+                           defined $room_config->{topic} and die "Expected not to find a topic";
+                        }
+
+                        $seen{$alias_local} = 1;
+                     }
+                  }
                }
-            }
-         }
 
-         foreach my $key ( keys %seen ) {
-            $seen{$key} or die "Did not find a /publicRooms result for $key";
-         }
+               foreach my $key ( keys %seen ) {
+                  $seen{$key} or die "Did not find a /publicRooms result for $key";
+               }
 
-         Future->done(1);
+               Future->done(1);
+            })
+         })
       })
    };
 
