dee61dfbbbb3e3264f0a03c567ee63a857b68b14
Richard van der Hoff <richard@matrix.org>
2017-08-23 18:41:45 +0100

    Use $synapse->{pid} instead of gut-wrenching

--- tests/05synapse.pl
+++ tests/05synapse.pl
@@ -33,7 +33,7 @@ main::AT_END sub {
       my $synapse = $_;
 
       # skip this if the process never got started.
-      return Future->done unless $synapse->{proc};
+      return Future->done unless $synapse->{pid};
 
       $OUTPUT->diag( "Killing ${\ $synapse->pid }" );
 
