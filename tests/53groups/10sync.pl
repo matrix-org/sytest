fa0749fc80950ec537bcc11fdd904e67bd28c83e
Erik Johnston <erikj@matrix.org>
2017-09-21 14:10:12 +0100

    Add tests

--- tests/53groups/10sync.pl
+++ tests/53groups/10sync.pl
@@ -19,6 +19,8 @@ test "Local group invites come down sync",
 
          matrix_sync( $user );
       })->then( sub {
+         matrix_sync_again( $user )
+      })->then( sub {
          matrix_invite_group_users( $creator, $group_id, $user );
       })->then( sub {
          try_repeat_until_success( sub {
@@ -66,6 +68,48 @@ test "Group creator sees group in sync",
 
          try_repeat_until_success( sub {
             matrix_sync_again( $creator )
+            ->then( sub {
+               my ( $body ) = @_;
+
+               assert_json_keys( $body, qw( groups ) );
+               assert_json_keys( $body->{groups}, qw( join ) );
+
+               assert_json_keys(  $body->{groups}{join}, $group_id );
+
+               Future->done( $body->{groups}{join}{$group_id} );
+            });
+         })
+      })->then( sub {
+         my ( $join ) = @_;
+
+         log_if_fail "Group Join", $join;
+
+         # TODO: Add check that profile comes down
+         # assert_json_keys( $join, qw( profile ) );
+         # assert_json_keys( $join->{profile}, qw( name ) );
+         # assert_eq( $join->{profile}{name}, $group_name );
+
+         Future->done( 1 );
+      });
+   };
+
+test "Group creator sees group in initial sync",
+   requires => [ local_admin_fixture( with_events => 0 ) ],
+
+   do => sub {
+      my ( $creator ) = @_;
+
+      my $group_id;
+
+      my $group_name = "Test Group";
+
+      matrix_create_group( $creator,
+         name => $group_name,
+      )->then( sub {
+         ( $group_id ) = @_;
+
+         try_repeat_until_success( sub {
+             matrix_sync( $creator )
             ->then( sub {
                my ( $body ) = @_;
 
