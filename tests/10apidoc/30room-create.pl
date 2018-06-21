e2757f8e4bd84c57abce69663a9845e8d3247c8c
Travis Ralston <travpc@gmail.com>
2018-06-21 09:55:12 -0600

    Remove "create room with avatar" test
    The original spec proposal (https://github.com/matrix-org/matrix-doc/pull/1326) has instead gone the route of deprecating the name and topic. The avatar test is not needed.
    
    Signed-off-by: Travis Ralston <travpc@gmail.com>

--- tests/10apidoc/30room-create.pl
+++ tests/10apidoc/30room-create.pl
@@ -163,47 +163,6 @@ test "POST /createRoom makes a room with a topic",
       });
    };
 
-test "POST /createRoom makes a room with an avatar",
-   requires => [ $user_fixture, local_user_fixture(),
-                 qw( can_create_private_room )],
-
-   proves => [qw( can_createroom_with_avatar )],
-
-   do => sub {
-      my ( $user ) = @_;
-
-      do_request_json_for( $user,
-         method => "POST",
-         uri    => "/r0/createRoom",
-
-         content => {
-            avatar => "mxc://localhost/test_val"
-         },
-      )->then( sub {
-         my ( $body ) = @_;
-
-         assert_json_keys( $body, qw( room_id ));
-         assert_json_nonempty_string( $body->{room_id} );
-
-         my ( $room_id ) = $body->{room_id};
-
-         do_request_json_for( $user,
-            method => "GET",
-            uri    => "/r0/rooms/$room_id/state/m.room.avatar",
-         )
-      })->then( sub {
-         my ( $state ) = @_;
-
-         log_if_fail "state", $state;
-
-         assert_json_keys( $state, qw( url ));
-         assert_json_nonempty_string( $state->{url} );
-         assert_eq( $state->{url}, "mxc://localhost/test_val", "room avatar" );
-
-         Future->done(1);
-      });
-   };
-
 test "Can /sync newly created room",
    requires => [ $user_fixture ],
 
