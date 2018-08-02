447e236c3041870fb3b7afe4ab09d44596c8d0ba
Richard van der Hoff <richard@matrix.org>
2018-08-02 13:49:35 +0100

    Check that federation endpoints check room id
    (patch supplied by @turt2live)

--- tests/50federation/34room-backfill.pl
+++ tests/50federation/34room-backfill.pl
@@ -186,3 +186,45 @@ test "Inbound federation can backfill events",
          Future->done(1);
       });
    };
+
+test "Backfill checks the events requested belong to the room",
+   requires => [ $main::OUTBOUND_CLIENT, $main::HOMESERVER_INFO[0],
+                 local_user_and_room_fixtures(),
+                 local_user_and_room_fixtures(),
+                 federation_user_id_fixture() ],
+   do => sub {
+      my ( $outbound_client, $info, $priv_creator, $priv_room_id,
+           $pub_creator, $pub_room_id, $fed_user_id ) = @_;
+      my $first_home_server = $info->server_name;
+
+      my $local_server_name = $outbound_client->server_name;
+
+      my $priv_join_event;
+
+      # Join the public room, but don't touch the private one
+      $outbound_client->join_room(
+         server_name => $first_home_server,
+         room_id     => $pub_room_id,
+         user_id     => $fed_user_id,
+      )->then( sub {
+         # Send an event into the private room
+         matrix_send_room_text_message( $priv_creator, $priv_room_id,
+            body => "Hello world",
+         )
+      })->then( sub {
+         my ( $priv_event_id ) = @_;
+
+         # We specifically use the public room, but the private event ID
+         # That's the point of this test.
+         $outbound_client->do_request_json(
+            method   => "GET",
+            hostname => $first_home_server,
+            uri      => "/backfill/$pub_room_id/",
+
+            params => {
+               v     => $priv_event_id,
+               limit => 1,
+            }
+         )->main::expect_http_403;
+      });
+   };
