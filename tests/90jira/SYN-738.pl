ea5d4a35570a56e5de943099b2b360aab0c1bc4f
Amber Brown <hawkowl@atleastfornow.net>
2018-08-09 12:13:46 +1000

    move the other

--- tests/90jira/SYN-738.pl
+++
@@ -1,9 +0,0 @@
-test "User signups are forbidden from starting with '_'",
-   requires => [ $main::API_CLIENTS[0] ],
-
-   do => sub {
-      my ( $http ) = @_;
-
-      matrix_register_user( $http, "_badname_here" )
-         ->main::expect_http_4xx;
-   };
