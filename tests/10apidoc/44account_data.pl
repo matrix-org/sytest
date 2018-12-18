b40317b1ef762660dbf3f47ed9dad923499a3042
Travis Ralston <travpc@gmail.com>
2018-12-17 19:47:13 -0700

    Add tests for reading account data
    See https://github.com/matrix-org/synapse/pull/4303

--- tests/10apidoc/44account_data.pl
+++ tests/10apidoc/44account_data.pl
@@ -41,6 +41,42 @@ sub matrix_add_room_account_data
    );
 }
 
+=head2 matrix_get_account_data
+
+   matrix_get_account_data( $user, $type, $content )->get;
+
+Get account data for the user.
+
+=cut
+
+sub matrix_get_account_data
+{
+   my ( $user, $type ) = @_;
+
+   do_request_json_for( $user,
+      method  => "GET",
+      uri     => "/r0/user/:user_id/account_data/$type",
+   );
+}
+
+=head2 matrix_get_room_account_data
+
+    matrix_get_account_data( $user, $room_id, $type, $content )->get;
+
+Get account data for the user for a room.
+
+=cut
+
+sub matrix_get_room_account_data
+{
+   my ( $user, $room_id, $type, $content ) = @_;
+
+   do_request_json_for( $user,
+      method  => "GET",
+      uri     => "/r0/user/:user_id/rooms/$room_id/account_data/$type",
+   );
+}
+
 sub matrix_add_filler_account_data_synced
 {
    my ( $user ) = @_;
