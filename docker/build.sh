5d953f7f307199150b5f28b26b9a65c78357dfa4
Andrew Morgan <andrew@amorgan.xyz>
2019-01-17 16:02:40 +0000

    Add dendrite dockerfile to build script

--- docker/build.sh
+++ docker/build.sh
@@ -2,3 +2,4 @@
 docker build ../ -f Dockerfile -t matrixdotorg/sytest
 docker build ../ -f Dockerfile-synapsepy2 -t matrixdotorg/sytest-synapsepy2
 docker build ../ -f Dockerfile-synapsepy3 -t matrixdotorg/sytest-synapsepy3
+docker build ../ -f Dockerfile-dendrite -t matrixdotorg/sytest-dendrite
