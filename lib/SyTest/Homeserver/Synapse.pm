d845013ebf66e101247c6fad627373bc1bec66f9
Brendan Abolivier <babolivier@matrix.org>
2019-03-15 15:44:46 +0000

    Typo

--- lib/SyTest/Homeserver/Synapse.pm
+++ lib/SyTest/Homeserver/Synapse.pm
@@ -167,7 +167,7 @@ sub start
             account => {
                 per_second => 1000,
                 burst_count => 1000,
-            }
+            },
             failed_attempts => {
                 per_second => 1000,
                 burst_count => 1000,
