8e54c9f2fb4e6456477093762c16a7e15e265ca6
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2019-03-18 15:17:04 +0000

    Add test for copying push rules on room upgrade (#571)

--- tests/30rooms/60version_upgrade.pl
+++ tests/30rooms/60version_upgrade.pl
@@ -454,6 +454,58 @@ test "/upgrade copies ban events to the new room",
       });
    };
 
+test "/upgrade copies push rules to the new room",
+   requires => [
+      local_user_and_room_fixtures(),
+      qw( can_upgrade_room_version ),
+   ],
+
+   do => sub {
+      my ( $creator, $room_id ) = @_;
+      my ( $new_room_id );
+
+      matrix_add_push_rule( $creator, "global", "room", $room_id, {
+         actions => [ "notify" ] 
+      })->then( sub {
+         matrix_sync( $creator );
+      })->then( sub {
+         upgrade_room_synced(
+            $creator, $room_id,
+            new_version => $TEST_NEW_VERSION,
+         );
+      })->then( sub {
+         ( $new_room_id, ) = @_;
+
+         matrix_get_push_rules( $creator )->then( sub {
+            my ( $body ) = @_;
+
+            my @to_check;
+
+            foreach my $kind ( keys %{ $body->{global} } ) {
+               foreach my $rule ( @{ $body->{global}{$kind} } ) {
+                  push @to_check, [ $kind, $rule->{rule_id} ];
+               }
+            }
+
+            my $found = 0;
+            try_repeat {
+               my $to_check = shift;
+
+               my ( $kind, $rule_id ) = @$to_check;
+
+               log_if_fail("testing $rule_id against $new_room_id");
+
+               if ( $rule_id eq $new_room_id ) {
+                  $found = 1;
+               }
+            } foreach => \@to_check;
+
+            if ( $found == 1 ) {
+               Future->done(1);
+            }
+         })
+      });
+   };
 
 test "/upgrade moves aliases to the new room",
    requires => [
