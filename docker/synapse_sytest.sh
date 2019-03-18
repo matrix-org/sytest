6cdf8eaa874db651a1a1ffbcbca9c22feb758e93
Erik Johnston <erik@matrix.org>
2019-03-18 16:23:06 +0000

    Install converage inside the docker image

--- docker/synapse_sytest.sh
+++ docker/synapse_sytest.sh
@@ -44,8 +44,7 @@ then
 fi
 
 # We've already created the virtualenv, but lets double check we have all deps.
-/venv/bin/pip install -q --no-cache-dir -e /src/
-/venv/bin/pip install -q --no-cache-dir coverage
+/venv/bin/pip install -q --upgrade --no-cache-dir -e /src/
 
 # Make sure all Perl deps are installed -- this is done in the docker build so will only install packages added since the last Docker build
 dos2unix ./install-deps.pl
