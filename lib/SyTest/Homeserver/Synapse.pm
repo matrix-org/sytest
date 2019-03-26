bbc16923d78f9633a1b882149ebc85033165f31c
Richard van der Hoff <richard@matrix.org>
2019-03-26 10:57:49 +0000

    Make replication torture a cmdline opt

--- lib/SyTest/Homeserver/Synapse.pm
+++ lib/SyTest/Homeserver/Synapse.pm
@@ -201,11 +201,6 @@ sub start
         # so this is effectively the minimum.
         bcrypt_rounds => 4,
 
-        # torture the replication protocol a bit, to replicate bugs.
-        # (value is the number of ms to wait before sending out each batch of
-        # updates.)
-        replication_torture_level => 50,
-
         # If we're using dendron-style split workers, we need to disable these
         # things in the main process
         start_pushers         => ( not $self->{dendron} ),
@@ -231,6 +226,7 @@ sub start
         map {
            defined $self->{$_} ? ( $_ => $self->{$_} ) : ()
         } qw(
+           replication_torture_level
            cas_config
            app_service_config_files
         ),
@@ -597,6 +593,12 @@ sub _init
    $self->SUPER::_init( @_ );
 
    $self->{dendron} = delete $args->{dendron_binary};
+   if( delete $args->{torture_replication} ) {
+      # torture the replication protocol a bit, to replicate bugs.
+      # (value is the number of ms to wait before sending out each batch of
+      # updates.)
+      $self->{replication_torture_level} = 50;
+   }
 
    my $idx = $self->{hs_index};
    $self->{ports}{dendron} = main::alloc_port( "dendron[$idx]" );
