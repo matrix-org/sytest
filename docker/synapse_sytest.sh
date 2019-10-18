a37883d7eab22fed2b797fafdfba8973b6920899
Andrew Morgan <andrew@amorgan.xyz>
2019-10-19 00:49:00 +0200

    Put correct path to script

--- docker/synapse_sytest.sh
+++ docker/synapse_sytest.sh
@@ -88,7 +88,7 @@ export TOP=/src
 
 if [ $TEST_STATUS -ne 0 ]; then
     # Build the annotation
-    perl ./format_tap.pl /logs/results.tap "$BUILDKITE_LABEL" >/logs/annotate.md
+    perl ./scripts/format_tap.pl /logs/results.tap "$BUILDKITE_LABEL" >/logs/annotate.md
 fi
 
 exit $TEST_STATUS
