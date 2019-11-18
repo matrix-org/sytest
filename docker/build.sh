058d47be5d7886064c449995f8b98809ecb6192c
Amber H. Brown <hawkowl@atleastfornow.net>
2019-11-19 04:06:14 +1100

    update dockerfiles

--- docker/build.sh
+++ docker/build.sh
@@ -7,5 +7,5 @@ docker build --pull ../ -f base-stretch.Dockerfile -t matrixdotorg/sytest:stretc
 docker build --pull ../ -f base-buster.Dockerfile -t matrixdotorg/sytest:buster
 docker build ../ -f synapse-py35.Dockerfile -t matrixdotorg/sytest-synapse:py35
 docker build ../ -f synapse-py37.Dockerfile -t matrixdotorg/sytest-synapse:py37
-docker build ../ -f dendrite-go110.Dockerfile -t matrixdotorg/sytest-dendrite:go110 -t matrixdotorg/sytest-dendrite:latest
+docker build ../ -f dendrite-go111.Dockerfile -t matrixdotorg/sytest-dendrite:go111 -t matrixdotorg/sytest-dendrite:latest
 docker build ../ -f dendrite-go113.Dockerfile -t matrixdotorg/sytest-dendrite:go113
