45e9a2caa8f416cd54c839d642957c14a1d85671
Andrew Morgan <andrew@amorgan.xyz>
2019-12-17 18:06:36 +0000

    Debugging

--- scripts/dendrite_sytest.sh
+++ scripts/dendrite_sytest.sh
@@ -52,7 +52,12 @@ fi
 echo >&2 "--- Copying assets"
 
 # Copy out the logs
-rsync --ignore-missing-args --min-size=1B -av server-0 server-1 /logs --include "*/" --include="*.log.*" --include="*.log" --include="dendrite-logs/*.log" --exclude="*"
+lsb_release -a
+pwd
+apt update
+apt install tree
+tree /work
+rsync --ignore-missing-args --min-size=1B -av /work/server-0 /work/server-1 /logs --include "*/" --include="*.log.*" --include="*.log" --exclude="*"
 
 if [ $TEST_STATUS -ne 0 ]; then
     # Build the annotation
