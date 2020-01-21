7fbbcdc4c8bc2ca2f5228c8e2a70311eaf6328be
Andrew Morgan <andrew@amorgan.xyz>
2020-01-21 17:13:03 +0000

    Add blacklist file

--- scripts/dendrite_sytest.sh
+++ scripts/dendrite_sytest.sh
@@ -40,7 +40,8 @@ echo >&2 "+++ Running tests"
 
 TEST_STATUS=0
 mkdir -p /logs
-./run-tests.pl -I Dendrite::Monolith -d /src/bin -W /src/testfile -O tap --all \
+./run-tests.pl -I Dendrite::Monolith -d /src/bin -O tap --all \
+    -W /src/testfile -B /src/sytest-blacklist \
     --work-directory="/work" \
     "$@" > /logs/results.tap || TEST_STATUS=$?
 
