ad88032bc44be21bf74ab2d6401be16a4dbe27bd
Andrew Morgan <andrew@amorgan.xyz>
2020-01-22 17:07:57 +0000

    copy fix

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -18,8 +18,8 @@ else
     if [ -n "$BUILDKITE_BRANCH" ]; then
         branch_name=$BUILDKITE_BRANCH
     else
-        # Otherwise, try and find out what the branch that the Synapse/Dendrite
-        # checkout is using. Fall back to develop if it's not a branch.
+        # Otherwise, try and find the branch that the Synapse/Dendrite checkout
+        # is using. Fall back to develop if unknown.
         branch_name="$(git --git-dir=/src/.git symbolic-ref HEAD 2>/dev/null)" || branch_name="develop"
     fi
 
