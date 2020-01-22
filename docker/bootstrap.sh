33fc516c996ac412f5a69ffe2454ea14249827cb
Andrew Morgan <andrew@amorgan.xyz>
2020-01-22 16:48:06 +0000

    Add some small comments and refactor others

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -13,14 +13,18 @@ if [ -d "/sytest" ]; then
 else
     echo "--- Trying to get same-named sytest branch..."
 
+    # Check if we're running in buildkite, if so it can tell us what
+    # Synapse/Dendrite branch we're running
     if [ -n "$BUILDKITE_BRANCH" ]; then
         branch_name=$BUILDKITE_BRANCH
     else
-        # Otherwise, try and find out what the branch that the Synapse/Dendrite checkout is using. Fall back to develop if it's not a branch.
+        # Otherwise, try and find out what the branch that the Synapse/Dendrite
+        # checkout is using. Fall back to develop if it's not a branch.
         branch_name="$(git --git-dir=/src/.git symbolic-ref HEAD 2>/dev/null)" || branch_name="develop"
 
         if [ "$SYTEST_TARGET" == "dendrite" ] && [ branch_name == "master" ]; then
-            # Dendrite uses master as its main branch. If the branch is master, we probably want sytest develop
+            # Dendrite uses master as its main branch. If the branch is master,
+            # we probably want sytest develop
             branch_name="develop"
         fi
     fi
