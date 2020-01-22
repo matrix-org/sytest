4894c28ee87e299cecb4b825cf780e0f3ce364c0
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2020-01-22 16:48:18 +0000

    Fix and move dendrite master branch check (#786)

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -18,11 +18,11 @@ else
     else
         # Otherwise, try and find out what the branch that the Synapse/Dendrite checkout is using. Fall back to develop if it's not a branch.
         branch_name="$(git --git-dir=/src/.git symbolic-ref HEAD 2>/dev/null)" || branch_name="develop"
+    fi
 
-        if [ "$SYTEST_TARGET" == "dendrite" ] && [ branch_name == "master" ]; then
-            # Dendrite uses master as its main branch. If the branch is master, we probably want sytest develop
-            branch_name="develop"
-        fi
+    if [ "$SYTEST_TARGET" == "dendrite" ] && [ "$branch_name" == "master" ]; then
+        # Dendrite uses master as its main branch. If the branch is master, we probably want sytest develop
+        branch_name="develop"
     fi
 
     # Try and fetch the branch
