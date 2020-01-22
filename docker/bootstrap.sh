ca275f9b45974e963684264279b64eda5976b544
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2020-01-22 17:08:13 +0000

    Fix incorrectly checking string literal instead of var (#785)

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -13,10 +13,13 @@ if [ -d "/sytest" ]; then
 else
     echo "--- Trying to get same-named sytest branch..."
 
-    if [ -n "BUILDKITE_BRANCH" ]; then
+    # Check if we're running in buildkite, if so it can tell us what
+    # Synapse/Dendrite branch we're running
+    if [ -n "$BUILDKITE_BRANCH" ]; then
         branch_name=$BUILDKITE_BRANCH
     else
-        # Otherwise, try and find out what the branch that the Synapse/Dendrite checkout is using. Fall back to develop if it's not a branch.
+        # Otherwise, try and find the branch that the Synapse/Dendrite checkout
+        # is using. Fall back to develop if unknown.
         branch_name="$(git --git-dir=/src/.git symbolic-ref HEAD 2>/dev/null)" || branch_name="develop"
     fi
 
