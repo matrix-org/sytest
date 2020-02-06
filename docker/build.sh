8fd660d3b2975e8d40d3fc3c3b8b5e8a6f4aa227
Neil Alexander <neilalexander@users.noreply.github.com>
2020-02-06 14:16:15 +0000

    Don't push Dendrite images to Docker Hub (#797)
    * Update build.sh
    
    * Update push.sh
    
    * Remove GO_VERSION so we use the default from the dockerfile instead
    
    * Remove go111 from Docker readme

--- docker/build.sh
+++ docker/build.sh
@@ -9,5 +9,4 @@ docker build --pull ../ -f base.Dockerfile --build-arg DEBIAN_VERSION=bullseye -
 docker build ../ -f synapse.Dockerfile --build-arg DEBIAN_VERSION=stretch -t matrixdotorg/sytest-synapse:py35
 docker build ../ -f synapse.Dockerfile --build-arg DEBIAN_VERSION=buster -t matrixdotorg/sytest-synapse:py37
 docker build ../ -f synapse.Dockerfile --build-arg DEBIAN_VERSION=bullseye -t matrixdotorg/sytest-synapse:py38
-docker build ../ -f dendrite.Dockerfile --build-arg DEBIAN_VERSION=stretch --build-arg GO_VERSION="1.11.13" -t matrixdotorg/sytest-dendrite:go111 
-docker build ../ -f dendrite.Dockerfile --build-arg DEBIAN_VERSION=stretch --build-arg GO_VERSION="1.13.7" -t matrixdotorg/sytest-dendrite:go113 -t matrixdotorg/sytest-dendrite:latest
+docker build ../ -f dendrite.Dockerfile --build-arg DEBIAN_VERSION=stretch -t matrixdotorg/sytest-dendrite:go113 -t matrixdotorg/sytest-dendrite:latest
