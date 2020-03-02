e6988f40b9a5d0c7fee729ab6d8091463c860848
Erik Johnston <erik@matrix.org>
2020-03-02 18:31:01 +0000

    Take 9

--- scripts/synapse_sytest.sh
+++ scripts/synapse_sytest.sh
@@ -21,7 +21,7 @@ if [ -n "$MULTI_POSTGRES" ]; then
 
     # We increase the max connections as we have more databases.
     for ver in /etc/postgresql/*/main; do
-        sed -r "s/^max_connections.*$/max_connections = 1000" $ver/postgresql.conf
+        sed -r "s/^max_connections.*$/max_connections = 1000/" $ver/postgresql.conf
     done
 
     # Start the database
