d80cb4be40a63d553e2628217e91b4118278c50f
Patrick Cloke <patrickc@matrix.org>
2020-04-29 08:25:23 -0400

    Add tests for floats.

--- tests/80torture/20json.pl
+++ tests/80torture/20json.pl
@@ -27,3 +27,51 @@ test "Invalid JSON integers",
          )->followed_by( \&main::expect_http_400 ),
       );
    };
+
+# Floats (including NaN, Infinity, and -Infinity) should be rejected.
+test "Invalid JSON floats",
+   requires => [ local_user_and_room_fixtures(
+      room_opts => { room_version => "org.matrix.strict_canonicaljson" }
+   ), ],
+
+   do => sub {
+      my ( $user, $room_id ) = @_;
+
+      Future->needs_all(
+         do_request_json_for( $user,
+            method  => "POST",
+            uri     => "/r0/rooms/$room_id/send/sytest.dummy",
+            content => {
+               msgtype => "sytest.dummy",
+               body    => 1.1,
+            },
+         )->followed_by( \&main::expect_http_400 ),
+
+         do_request_json_for( $user,
+            method  => "PUT",
+            uri     => "/r0/rooms/$room_id/send/sytest.dummy",
+            content => {
+               msgtype => "sytest.dummy",
+               body    => "NaN",
+            },
+         )->followed_by( \&main::expect_http_400 ),
+
+         do_request_json_for( $user,
+            method  => "PUT",
+            uri     => "/r0/rooms/$room_id/send/sytest.dummy",
+            content => {
+               msgtype => "sytest.dummy",
+               body    => "inf",
+            },
+         )->followed_by( \&main::expect_http_400 ),
+
+         do_request_json_for( $user,
+            method  => "PUT",
+            uri     => "/r0/rooms/$room_id/send/sytest.dummy",
+            content => {
+               msgtype => "sytest.dummy",
+               body    => "-inf",
+            },
+         )->followed_by( \&main::expect_http_400 ),
+      );
+   };
