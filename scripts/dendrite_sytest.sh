a7a2f846caee9e1152fe452beb30ecdcc2d78483
Kegan Dougal <kegan@matrix.org>
2020-05-01 16:14:06 +0100

    Maybe this time

--- scripts/dendrite_sytest.sh
+++ scripts/dendrite_sytest.sh
@@ -32,7 +32,7 @@ export POSTGRES_DB_2=pg2
 # Build dendrite
 echo >&2 "--- Building dendrite from source"
 cd /src
-go build -o bin/dendrite-monolith-server cmd/dendrite-monolith-server
+go build -o bin/dendrite-monolith-server ./cmd/dendrite-monolith-server
 cd -
 
 # Run the tests
