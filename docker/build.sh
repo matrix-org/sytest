21b81f7a57c9cce075ce7ae4bb8bcc9e9a5442af
Andrew Morgan <1342360+anoadragon453@users.noreply.github.com>
2020-05-15 10:13:04 +0100

    Fix the Dockerfile filenames for dinsic (#867)

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,5 @@
 set -ex
 
 cd $(dirname $0)
-docker build --pull ../ -f Dockerfile -t matrixdotorg/sytest:dinsic
-docker build ../ -t matrixdotorg/sytest-synapse:dinsic
+docker build --pull ../ -f base.Dockerfile -t matrixdotorg/sytest:dinsic
+docker build ../ -f synapse.Dockerfile -t matrixdotorg/sytest-synapse:dinsic
