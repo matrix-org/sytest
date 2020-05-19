82ae5cd2e3cd3123eada55e13c12f91170402ddb
Patrick Cloke <patrickc@matrix.org>
2020-05-19 15:07:33 -0400

    Clarify that invalid values aren't even JSON.

--- tests/80torture/20json.pl
+++ tests/80torture/20json.pl
@@ -28,7 +28,7 @@ test "Invalid JSON integers",
       );
    };
 
-# Floats (including NaN, Infinity, and -Infinity) should be rejected.
+# Floats should be rejected.
 test "Invalid JSON floats",
    requires => [ local_user_and_room_fixtures(
       room_opts => { room_version => "6" }
@@ -37,16 +37,31 @@ test "Invalid JSON floats",
    do => sub {
       my ( $user, $room_id ) = @_;
 
-      Future->needs_all(
-         do_request_json_for( $user,
-            method  => "POST",
-            uri     => "/r0/rooms/$room_id/send/sytest.dummy",
-            content => {
-               msgtype => "sytest.dummy",
-               body    => 1.1,
-            },
-         )->followed_by( \&main::expect_http_400 ),
+      do_request_json_for( $user,
+         method  => "POST",
+         uri     => "/r0/rooms/$room_id/send/sytest.dummy",
+         content => {
+            msgtype => "sytest.dummy",
+            body    => 1.1,
+         },
+      )->followed_by( \&main::expect_http_400 );
+   };
 
+# Special values (like inf/nan) should be rejected. Note that these values are
+# not technically valid JSON, but extensions that some programming languages
+# support automatically.
+test "Invalid JSON special values",
+   requires => [ local_user_and_room_fixtures(
+      room_opts => { room_version => "6" }
+   ), ],
+
+   do => sub {
+      my ( $user, $room_id ) = @_;
+
+      my $http = $user->http;
+
+      Future->needs_all(
+         # Try some Perl magic values.
          do_request_json_for( $user,
             method  => "POST",
             uri     => "/r0/rooms/$room_id/send/sytest.dummy",
@@ -72,6 +87,37 @@ test "Invalid JSON floats",
                msgtype => "sytest.dummy",
                body    => "-inf" + 0,
             },
+         )->followed_by( \&main::expect_http_400 ),
+
+         # Try some Python magic values.
+         $user->http->do_request(
+            method       => "POST",
+            uri          => "/r0/rooms/$room_id/send/sytest.dummy",
+            params       => {
+               access_token => $user->access_token,
+            },
+            content      => '{"msgtype": "sytest.dummy", "body": Infinity}',
+            content_type => "application/json",
+         )->followed_by( \&main::expect_http_400 ),
+
+         $user->http->do_request(
+            method       => "POST",
+            uri          => "/r0/rooms/$room_id/send/sytest.dummy",
+            params       => {
+               access_token => $user->access_token,
+            },
+            content      => '{"msgtype": "sytest.dummy", "body": -Infinity}',
+            content_type => "application/json",
+         )->followed_by( \&main::expect_http_400 ),
+
+         $user->http->do_request(
+            method       => "POST",
+            uri          => "/r0/rooms/$room_id/send/sytest.dummy",
+            params       => {
+               access_token => $user->access_token,
+            },
+            content      => '{"msgtype": "sytest.dummy", "body": NaN}',
+            content_type => "application/json",
          )->followed_by( \&main::expect_http_400 ),
       );
    };
