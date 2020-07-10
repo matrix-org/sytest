13b21f4a629cca45c233027eda50fe7856c60372
Patrick Cloke <patrickc@matrix.org>
2020-07-10 08:35:07 -0400

    Allow the earliest event to be the member event or any previous events.

--- tests/50federation/33room-get-missing-events.pl
+++ tests/50federation/33room-get-missing-events.pl
@@ -484,8 +484,12 @@ test "Outbound federation will ignore a missing event with bad JSON for room ver
             assert_json_list( my $earliest = $body->{earliest_events} );
             @$earliest == 1 or
                die "Expected a single 'earliest_event' ID";
-            assert_eq( $earliest->[0], $room->id_for_event( $latest_event ),
-               'earliest_events[0]' );
+            # It is expected that the earliest event is the m.room.member event,
+            # but it is possible that the caches have not yet been invalidated
+            # so also allow any of that event's previous events.
+            my @expected = @{$latest_event->{prev_events}};
+            push( @expected, $room->id_for_event( $latest_event ) );
+            assert_in_list( $earliest->[0], @expected, 'earliest_events[0]' );
 
             assert_json_list( my $latest = $body->{latest_events} );
             @$latest == 1 or
