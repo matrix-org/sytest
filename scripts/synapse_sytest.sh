68473946a0153086893d9d33d578e1749d054504
Richard van der Hoff <1389908+richvdh@users.noreply.github.com>
2020-08-06 01:30:46 +0100

    Add redis support to synapse docker image (#928)
    ... because I kept forgetting how to do the networking foo to get redis
    running, and because it seems to make sense now that TCP replication is
    deprecated.

--- scripts/synapse_sytest.sh
+++ scripts/synapse_sytest.sh
@@ -15,6 +15,11 @@ cd "$(dirname $0)/.."
 
 mkdir /work
 
+# start the redis server, if desired
+if [ -n "$REDIS" ]; then
+    /usr/bin/redis-server /etc/redis/redis.conf
+fi
+
 # PostgreSQL setup
 if [ -n "$MULTI_POSTGRES" ] || [ -n "$POSTGRES" ]; then
     sed -i -r "s/^max_connections.*$/max_connections = 500/" "$PGDATA/postgresql.conf"
@@ -163,6 +168,10 @@ if [ -n "$WORKERS" ]; then
     RUN_TESTS+=(-I Synapse::ViaHaproxy --dendron-binary=/pydron.py)
 else
     RUN_TESTS+=(-I Synapse)
+fi
+
+if [ -n "$REDIS" ]; then
+    RUN_TESTS+=(--redis-host=localhost)
 fi
 
 mkdir -p /logs
