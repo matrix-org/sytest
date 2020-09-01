cac05b1458e5c69a46a3afb75984b1dfab16d727
Matthew Hodgson <matthew@matrix.org>
2020-09-01 23:19:08 +0100

    add failing test for ensuring peeks require world_readable history visibility

--- tests/31sync/17peeking.pl
+++ tests/31sync/17peeking.pl
@@ -2,13 +2,15 @@ use Future::Utils qw( repeat );
 
 # Tests MSC2753 style peeking
 
-test "Local users can peek by room ID",
+test "Local users can peek into world_readable rooms by room ID",
    requires => [ local_user_and_room_fixtures(), local_user_fixture() ],
 
    check => sub {
       my ( $user, $room_id, $peeking_user ) = @_;
 
-      matrix_send_room_text_message_synced( $user, $room_id, body => "something to peek")->then(sub {
+      matrix_set_room_history_visibility( $user, $room_id, "world_readable" )->then(sub {
+         matrix_send_room_text_message_synced( $user, $room_id, body => "something to peek");
+      })->then(sub {
          do_request_json_for( $peeking_user,
             method => "POST",
             uri    => "/r0/peek/$room_id",
@@ -42,7 +44,7 @@ test "Local users can peek by room ID",
          my $room = $body->{rooms}{peek}{$room_id};
          (!defined $room) or die "Unchanged rooms shouldn't be in the sync response";
       })->then( sub {
-         matrix_send_room_text_message_synced( $user, $room_id, body => "something else to peek")
+         matrix_send_room_text_message_synced( $user, $room_id, body => "something else to peek");
       })->then( sub {
          matrix_sync_again( $peeking_user );
       })->then( sub {
@@ -59,6 +61,31 @@ test "Local users can peek by room ID",
    };
 
 
+for my $visibility (qw(shared invited joined)) {
+   test "We can't peek into rooms with $visibility history_visibility",
+      requires => [ local_user_and_room_fixtures(), local_user_fixture() ],
+
+      check => sub {
+         my ( $user, $room_id, $peeking_user ) = @_;
+
+         matrix_set_room_history_visibility( $user, $room_id, $visibility )->then(sub {
+            do_request_json_for( $peeking_user,
+               method => "POST",
+               uri    => "/r0/peek/$room_id",
+               content => {},
+            );
+         })->main::expect_http_403()
+         ->then( sub {
+            my ( $response ) = @_;
+            my $body = decode_json( $response->content );
+            log_if_fail "error body", $body;
+            assert_eq( $body->{errcode}, "M_FORBIDDEN", 'responsecode' );
+            Future->done( 1 );
+         });
+      };
+}
+
+
 my $room_alias_name = sprintf("peektest-%s", $TEST_RUN_ID);
 test "Local users can peek by room alias",
    requires => [
@@ -69,7 +96,9 @@ test "Local users can peek by room alias",
    check => sub {
       my ( $user, $room_id, $peeking_user ) = @_;
 
-      matrix_send_room_text_message_synced( $user, $room_id, body => "something to peek")->then(sub {
+      matrix_set_room_history_visibility( $user, $room_id, "world_readable" )->then(sub {
+         matrix_send_room_text_message_synced( $user, $room_id, body => "something to peek");
+      })->then(sub {
          do_request_json_for( $peeking_user,
             method => "POST",
             uri    => "/r0/peek/#$room_alias_name:".$user->http->server_name,
@@ -88,7 +117,6 @@ test "Local users can peek by room alias",
       })
    };
 
-
 test "Peeked rooms only turn up in the sync for the device who peeked them",
    requires => [ local_user_and_room_fixtures(), local_user_fixture() ],
 
@@ -96,9 +124,11 @@ test "Peeked rooms only turn up in the sync for the device who peeked them",
       my ( $user, $room_id, $peeking_user ) = @_;
       my ( $peeking_user_device2 );
 
-      matrix_login_again_with_user($peeking_user)->then(sub {
+      matrix_set_room_history_visibility( $user, $room_id, "world_readable" )->then(sub {
+         matrix_login_again_with_user($peeking_user);
+      })->then(sub {
          $peeking_user_device2 = $_[0];
-         matrix_send_room_text_message_synced( $user, $room_id, body => "something to peek")
+         matrix_send_room_text_message_synced( $user, $room_id, body => "something to peek");
       })->then(sub {
          do_request_json_for( $peeking_user,
             method => "POST",
@@ -136,10 +166,10 @@ test "Peeked rooms only turn up in the sync for the device who peeked them",
       })
    };
 
-# test "We can't peek into private rooms"
-
 # test "Users can unpeek from rooms"
 
 # test "Joining a peeked room moves it atomically from peeked to joined rooms and stops peeking"
 
 # test "Parting a room which was joined after being peeked"
+
+# test "Changing history visibility to non-world_readable terminates peeks"
