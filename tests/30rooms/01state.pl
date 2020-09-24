462418ad536f8bd4fb500e7bb5b8969e66f2536d
Neil Alexander <neilalexander@users.noreply.github.com>
2020-09-24 12:09:14 +0100

    Remove warning comments

--- tests/30rooms/01state.pl
+++ tests/30rooms/01state.pl
@@ -277,7 +277,6 @@ sub set_test_state
 }
 
 test "Setting state twice is idempotent",
-   # TODO: deprecated endpoint used in this test
    # Setting synced to 1 inserts a m.room.test object into the
    # timeline which this test does not expect
    requires => [ local_user_and_room_fixtures( room_opts => { synced => 0 } ) ],
@@ -302,7 +301,6 @@ test "Setting state twice is idempotent",
    };
 
 test "Joining room twice is idempotent",
-   # TODO: deprecated endpoint used in this test
    requires => [ local_user_and_room_fixtures() ],
 
    check => sub {
