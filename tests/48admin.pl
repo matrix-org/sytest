e345c7298be727636689b5b24b8b0887e622f04b
Dirk Klimpel <5740567+dklimpel@users.noreply.github.com>
2020-12-10 13:30:02 +0100

    Switch `shutdown_room` to `delete` room admin API (#988)
    In preparation for removing the deprecated `shutdown_room` API.

--- tests/48admin.pl
+++ tests/48admin.pl
@@ -382,8 +382,12 @@ multi_test "Shutdown room",
       })->then( sub {
          do_request_json_for( $admin,
             method   => "POST",
-            full_uri => "/_synapse/admin/v1/shutdown_room/$room_id",
-            content  => { "new_room_user_id" => $dummy_user->user_id },
+            full_uri => "/_synapse/admin/v1/rooms/$room_id/delete",
+            content  => {
+               new_room_user_id => $dummy_user->user_id,
+               block => JSON::true,
+               purge => JSON::false,
+            },
          );
       })->SyTest::pass_on_done( "Shutdown room returned success" )
       ->then( sub {
