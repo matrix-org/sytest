d0de4de53c558425179f02c916d6a2734dff4789
Nicolas Werner <nicolas.werner@hotmail.de>
2021-05-01 18:35:55 +0200

    Fix test failures if presence, account_data or to_device keys are empty
    Signed-off-by: Nicolas Werner <nicolas.werner@hotmail.de>

--- tests/46direct/01directmessage.pl
+++ tests/46direct/01directmessage.pl
@@ -70,7 +70,7 @@ sub matrix_recv_device_message
       return 1 if $f->failure;
       my $resp = $f->get;
       log_if_fail "Sync response", $resp;
-      if( exists $resp->{to_device}{events} ) {
+      if( exists $resp->{to_device} and exists $resp->{to_device}{events} ) {
         return scalar @{ $resp->{to_device}{events} };
       }
    };
