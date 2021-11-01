a2b0827dbdef078ab846cb3fe8ec1d129baea5de
Kegan Dougal <kegan@matrix.org>
2021-11-01 11:44:54 +0000

    Move SYN-442 test to invites file; make it use v2 /sync

--- tests/30rooms/06invite.pl
+++ tests/30rooms/06invite.pl
@@ -409,3 +409,74 @@ test "Users cannot invite a user that is already in the room",
             ->main::expect_http_403;
       });
    };
+
+multi_test "Test that we can be reinvited to a room we created",
+   requires => [ local_user_fixture( with_events => 1 ), remote_user_fixture( with_events => 1 ),
+                 qw( can_change_power_levels )],
+
+   check => sub {
+      my ( $user_1, $user_2 ) = @_;
+
+      my $room_id;
+
+      matrix_create_room( $user_1 )
+         ->SyTest::pass_on_done( "User A created a room" )
+      ->then( sub {
+         ( $room_id ) = @_;
+
+         matrix_put_room_state( $user_1, $room_id,
+            type    => "m.room.join_rules",
+            content => { join_rule => "invite" },
+         )->SyTest::pass_on_done( "User A set the join rules to 'invite'" )
+      })->then( sub {
+
+         matrix_invite_user_to_room( $user_1, $user_2, $room_id )
+            ->SyTest::pass_on_done( "User A invited user B" )
+      })->then( sub {
+
+         await_sync( $user_2, check => sub {
+            my ( $body ) = @_;
+
+            return 0 unless exists $body->{rooms}{invite}{$room_id};
+            return $body->{rooms}{invite}{$room_id};
+         })->SyTest::pass_on_done( "User B received the invite from A" )
+      })->then( sub {
+
+         matrix_join_room( $user_2, $room_id )
+            ->SyTest::pass_on_done( "User B joined the room" )
+      })->then( sub {
+
+         matrix_change_room_power_levels( $user_1, $room_id, sub {
+            my ( $levels ) = @_;
+
+            $levels->{users}{ $user_2->user_id } = 100;
+         })->SyTest::pass_on_done( "User A set user B's power level to 100" )
+      })->then( sub {
+
+         matrix_leave_room( $user_1, $room_id )
+            ->SyTest::pass_on_done( "User A left the room" )
+      })->then( sub {
+         await_sync_timeline_contains( $user_2, $room_id,  check => sub {
+            my ( $event ) = @_;
+            return $event->{type} eq "m.room.member" &&
+               $event->{content}->{membership} eq "leave";
+         })->SyTest::pass_on_done( "User B received the leave event" )
+      })->then( sub {
+
+         matrix_invite_user_to_room( $user_2, $user_1, $room_id )
+            ->SyTest::pass_on_done( "User B invited user A back to the room" )
+      })->then( sub {
+
+         await_sync( $user_1, check => sub {
+            my ( $body ) = @_;
+
+            return 0 unless exists $body->{rooms}{invite}{$room_id};
+            return $body->{rooms}{invite}{$room_id};
+         })->SyTest::pass_on_done( "User A received the invite from user B" )
+      })->then( sub {
+
+         retry_until_success {
+            matrix_join_room( $user_1, $room_id )
+         }->SyTest::pass_on_done( "User A joined the room" )
+      })->then_done(1);
+   };
