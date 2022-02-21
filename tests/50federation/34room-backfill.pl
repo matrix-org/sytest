b4dcf8c556934ea3214243e0402aa092a9698c6f
Richard van der Hoff <richard@matrix.org>
2022-02-21 18:09:06 +0000

    Handle servers which decide to make `/state` requests
    Some server implementations may make `/state` requests when there is a large
    amount of state to sync up; add code to handle that.

--- tests/50federation/34room-backfill.pl
+++ tests/50federation/34room-backfill.pl
@@ -363,15 +363,48 @@ test "Backfilled events whose prev_events are in a different room do not allow c
             destination => $synapse_server_name,
          );
       })->then( sub {
-         # wait for S to arrive
+         # the server may send a /state request; be prepared to answer that.
+         # (it may, alternatively, send individual /event requests)
+         my $state_req_fut = $inbound_server->await_request_v1_state(
+            $room2->{room_id}, $event_id_Q,
+         )->then( sub {
+            my ( $req, @params ) = @_;
+            log_if_fail "/state request", \@params;
+
+
+            my %state  = %{ $room2->{current_state} };
+            my $resp = {
+               state => [ values( %state ) ],
+
+               # XXX we're supposed to return the whole auth chain here,
+               # not just Q's auth_events. It doesn't matter too much
+               # here though.
+               auth_chain => [
+                  map { $inbound_server->datastore->get_event( $_ ) } @{ $room2->event_ids_from_refs( $event_Q->{auth_events} ) },
+               ],
+            };
+
+            log_if_fail "/state response", $resp;
+            $req->respond_json( $resp );
+
+            # return a future which never completes, so that wait_any is not
+            # satisfied.
+            return Future->new();
+         });
+
          log_if_fail "Awating arrival of event S $event_id_S in room $room2_id";
 
-         await_sync_timeline_contains(
-            $creator_user, $room2_id,
-            check => sub {
-               $_[0]->{event_id} eq $event_id_S
-            },
-         );
+         # wait for either S to turn up in /sync, or $state_req_fut to fail.
+         Future->wait_any(
+            $state_req_fut,
+
+            await_sync_timeline_contains(
+               $creator_user, $room2_id,
+               check => sub {
+                  $_[0]->{event_id} eq $event_id_S
+               },
+            ),
+        );
       })->then( sub {
          my $filter = $json->encode( { room => { timeline => { limit => 2 }}} );
          matrix_sync(
