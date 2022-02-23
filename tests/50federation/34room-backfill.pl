4e7d7ba2a28b88ac2d4865c31b61f8ab0223a3eb
Richard van der Hoff <richard@matrix.org>
2022-02-23 11:13:39 +0000

    handle  in *two* places

--- tests/50federation/34room-backfill.pl
+++ tests/50federation/34room-backfill.pl
@@ -338,7 +338,7 @@ test "Backfilled events whose prev_events are in a different room do not allow c
             $room2_id, $event_id_Q,
          )->then( sub {
             my ( $req, @params ) = @_;
-            log_if_fail "/state_ids request", \@params;
+            log_if_fail "/state_ids request (1)", \@params;
 
             my %state  = %{ $room2->{current_state} };
             my $resp = {
@@ -352,7 +352,7 @@ test "Backfilled events whose prev_events are in a different room do not allow c
                auth_chain_ids => $room2->event_ids_from_refs( $event_Q->{auth_events} ),
             };
 
-            log_if_fail "/state_ids response", $resp;
+            log_if_fail "/state_ids response (1)", $resp;
             $req->respond_json( $resp );
             Future->done(1);
          }),
@@ -363,14 +363,46 @@ test "Backfilled events whose prev_events are in a different room do not allow c
             destination => $synapse_server_name,
          );
       })->then( sub {
-         # wait for S to arrive
+         # the server may send a /state request; be prepared to answer that.
+         # (it may, alternatively, send individual /event requests)
+         my $state_req_fut = $inbound_server->await_request_v1_state(
+            $room2->{room_id}, $event_id_Q,
+         )->then( sub {
+            my ( $req, @params ) = @_;
+            log_if_fail "/state request (1)", \@params;
+
+            my %state  = %{ $room2->{current_state} };
+            my $resp = {
+               state => [ values( %state ) ],
+
+               # XXX we're supposed to return the whole auth chain here,
+               # not just Q's auth_events. It doesn't matter too much
+               # here though.
+               auth_chain => [
+                  map { $inbound_server->datastore->get_event( $_ ) } @{ $room2->event_ids_from_refs( $event_Q->{auth_events} ) },
+               ],
+            };
+
+            log_if_fail "/state response (1)", $resp;
+            $req->respond_json( $resp );
+
+            # return a future which never completes, so that wait_any is not
+            # satisfied.
+            return Future->new();
+         });
+
          log_if_fail "Awating arrival of event S $event_id_S in room $room2_id";
 
-         await_sync_timeline_contains(
-            $creator_user, $room2_id,
-            check => sub {
-               $_[0]->{event_id} eq $event_id_S
-            },
+         # wait for either S to turn up in /sync, or $state_req_fut to fail.
+         Future->wait_any(
+            $state_req_fut,
+
+            await_sync_timeline_contains(
+               $creator_user, $room2_id,
+               check => sub {
+                  $_[0]->{event_id} eq $event_id_S
+               },
+              ),
          );
       })->then( sub {
          my $filter = $json->encode( { room => { timeline => { limit => 2 }}} );
@@ -392,7 +424,7 @@ test "Backfilled events whose prev_events are in a different room do not allow c
             $room2_id, $event_id_Q,
          )->then( sub {
             my ( $req, @params ) = @_;
-            log_if_fail "/state_ids request", \@params;
+            log_if_fail "/state_ids request (2)", \@params;
 
             my %state  = %{ $room2->{current_state} };
             my $resp = {
@@ -402,7 +434,7 @@ test "Backfilled events whose prev_events are in a different room do not allow c
                auth_chain_ids => $room2->event_ids_from_refs( $event_Q->{auth_events} ),
             };
 
-            log_if_fail "/state_ids response", $resp;
+            log_if_fail "/state_ids response (2)", $resp;
             $req->respond_json( $resp );
 
             # return a future which never completes, so that wait_any is not
@@ -416,7 +448,7 @@ test "Backfilled events whose prev_events are in a different room do not allow c
             $room2->{room_id}, $event_id_Q,
          )->then( sub {
             my ( $req, @params ) = @_;
-            log_if_fail "/state request", \@params;
+            log_if_fail "/state request (2)", \@params;
 
 
             my %state  = %{ $room2->{current_state} };
@@ -431,7 +463,7 @@ test "Backfilled events whose prev_events are in a different room do not allow c
                ],
             };
 
-            log_if_fail "/state response", $resp;
+            log_if_fail "/state response (2)", $resp;
             $req->respond_json( $resp );
 
             # return a future which never completes, so that wait_any is not
