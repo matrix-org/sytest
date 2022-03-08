0160f5b5aaeb12ac4ec1fe078942af5b5b53b82a
David Robertson <davidr@element.io>
2022-03-08 17:51:48 +0000

    Do actually need pyproject

--- scripts/synapse_sytest.sh
+++ scripts/synapse_sytest.sh
@@ -122,7 +122,7 @@ fi
 if [ -d "$SYNAPSE_SOURCE" ]; then
     echo "Creating tarball from synapse source"
     tar -C "$SYNAPSE_SOURCE" -czf /tmp/synapse.tar.gz \
-        synapse README.rst
+        synapse README.rst pyproject.toml
     SYNAPSE_SOURCE="/tmp/synapse.tar.gz"
 elif [ ! -r "$SYNAPSE_SOURCE" ]; then
     echo "Unable to read synapse source at $SYNAPSE_SOURCE" >&2
