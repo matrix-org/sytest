10bbbc057380ac8e6e5aa67bdd08dc238192c298
David Robertson <davidr@element.io>
2023-03-02 12:26:02 +0000

    Synapse: fix the build and reduce logging noise (#1334)
    * Fix synapse-sytest:testing build
    
    * Reduce `poetry install` verbosity
    
    otherwise this means that `poetry install` tells about every single file
    it ignores, which includes loads of guff in `.mypy_cache` and
    `.ruff_cache` and others
    
    * Limit number of haproxy connections
    
    I don't know the full context, but
    https://github.com/haproxy/haproxy/issues/2043 seems to explain what's
    going on. The number `2000` was chosen arbitrarily.
    
    * Hopefully fix sytest docker build on bookworm

--- lib/SyTest/Homeserver/Synapse.pm
+++ lib/SyTest/Homeserver/Synapse.pm
@@ -1201,6 +1201,8 @@ global
     ssl-default-bind-ciphers "EECDH+ECDSA+AESGCM EECDH+aRSA+AESGCM EECDH+ECDSA+SHA384 EECDH+ECDSA+SHA256 EECDH+aRSA+SHA384 EECDH+aRSA+SHA256 EECDH+aRSA+RC4 EECDH EDH+aRSA RC4 !aNULL !eNULL !LOW !3DES !MD5 !EXP !PSK !SRP !DSS !RC4"
     ssl-default-bind-options no-sslv3
 
+    maxconn 2000
+
 defaults
     mode http
     log global
